cf6299088bc1a6a79873ac481e1cf6b52503782c
jgaffuri <jgaf@protonmail.com>
2019-12-01 21:02:53 +0100

    structure change

--- PixiJS/tests/csvTiles/globals.js
+++
@@ -1,5 +0,0 @@
-// global variables
-globals = {
-    initial_CSV_URL: "../assets/csv/tiles/pop_grid_2011_10km/"
-    //initial_CSV_URL : "https://raw.githubusercontent.com/eurostat/EuroGridLayer/master/PixiJS/tests/csvTiles/assets/csv/tiles/pop_grid_2011_10km/"
-};
