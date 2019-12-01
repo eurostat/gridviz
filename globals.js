16916451e0c6a6f67d16cc43d1715c15603742ee
jgaffuri <jgaf@protonmail.com>
2019-12-01 21:12:34 +0100

    update service URL

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 // global variables
 globals = {
-    initial_CSV_URL: "../assets/csv/tiles/pop_grid_2011_10km/"
-    //initial_CSV_URL : "https://raw.githubusercontent.com/eurostat/EuroGridLayer/master/PixiJS/tests/csvTiles/assets/csv/tiles/pop_grid_2011_10km/"
+    initial_CSV_URL: "assets/tiles/pop_grid_2011_10km/"
+    //initial_CSV_URL : "https://raw.githubusercontent.com/eurostat/EuroGridLayer/master/assets/tiles/pop_grid_2011_10km/"
 };
