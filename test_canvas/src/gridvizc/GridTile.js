eb8b80fc5f9068fe834a949cb2d4497729f96368
jgaffuri <jgaf@protonmail.com>
2021-11-19 23:58:36 +0100

    gridviz canvas test

---
+++ test_canvas/src/gridvizc/GridTile.js
@@ -0,0 +1,44 @@
+//@ts-check
+
+export class GridTile {
+
+
+    constructor(data, xT, yT, gridInfo) {
+
+        /** @type {Array} */
+        this.cells = data;
+        /** @type {number} */
+        this.x = xT
+        /** @type {number} */
+        this.y = yT
+
+        this.geoTile(gridInfo)
+    }
+
+
+
+
+    //convert cell position from tile position into geo position
+    geoTile(gridInfo) {
+
+        /** @type {number} */
+        const r = gridInfo.resolutionGeo;
+        /** @type {number} */
+        const s = gridInfo.tileSizeCell;
+        /** @type {number} */
+        const xMin = gridInfo.originPoint.x + r*s*this.x
+        /** @type {number} */
+        const yMin = gridInfo.originPoint.y + r*s*this.y
+
+        for(let i=0; i<this.cells.length; i++) {
+            const cell = this.cells[i];
+            /** @type {number} */
+            cell.x = xMin + cell.x * r;
+            /** @type {number} */
+            cell.y = yMin + cell.y * r;
+            //console.log(cell)
+        }
+    }
+
+
+}
