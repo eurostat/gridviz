07339fed799c603d51280777d08509c83a4620b1
jgaffuri <jgaf@protonmail.com>
2021-11-21 17:45:26 +0100

    test tiling with canvas

--- test_canvas/src/gridvizc/GridLayer.js
+++ test_canvas/src/gridvizc/GridLayer.js
@@ -2,6 +2,22 @@
 
 export class GridLayer {
 
+    /**
+     * @param {number} minZoom 
+     * @param {number} maxZoom 
+     */
+    constructor(minZoom, maxZoom) {
+
+        /** @type {number} */
+        this.minZoom = minZoom;
+        /** @type {number} */
+        this.maxZoom = maxZoom;
+
+    }
+
+    draw() {
+        //TODO
+    }
 
 
 }
