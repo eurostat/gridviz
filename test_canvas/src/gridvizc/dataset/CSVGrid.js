9c8c3c2b9f3e31fc769c72b7c37c5163444dd4b2
jgaffuri <jgaf@protonmail.com>
2021-11-22 12:12:08 +0100

    test tiling with canvas

---
+++ test_canvas/src/gridvizc/dataset/CSVGrid.js
@@ -0,0 +1,55 @@
+//@ts-check
+/** @typedef {{ dims: object, crs: string, tileSizeCell: number, originPoint: {x:number,y:number}, resolutionGeo: number, tilingBounds:Envelope }} GridInfo */
+
+import { csv } from "d3-fetch";
+import { Dataset, Cell, Envelope } from "../viewer/Dataset"
+
+/**
+ * A dataset composed of a single CSV file.
+ */
+export class CSVGrid extends Dataset {
+
+    /**
+     * @param {string} url This url of the info.json.
+     */
+    constructor(url) {
+        super(url, undefined)
+
+        /** @type {Array.<Cell>} */
+        this.cells = [];
+    }
+
+
+    /**
+     * Request data within a geographic envelope.
+     * 
+     * @param {Envelope} e 
+     * @param {function} callback 
+     */
+    getData(e, callback) {
+
+        //TODO
+
+        return this;
+    }
+
+
+    /**
+     * Get all cells from cache which are within a geographical envelope.
+     * 
+     * @param {Envelope} e 
+     * @returns {Array.<Cell>}
+     */
+    getCells(e) {
+
+        /** @type {Array.<Cell>} */
+        let cells = []
+
+        for (const cell of this.cells) {
+            //TODO
+        }
+
+        return cells;
+    }
+
+}
