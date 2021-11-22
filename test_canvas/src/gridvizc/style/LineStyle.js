d8da661c65cdb20c9ae138491d6d944afd164d38
jgaffuri <jgaf@protonmail.com>
2021-11-22 15:09:52 +0100

    test tiling with canvas

---
+++ test_canvas/src/gridvizc/style/LineStyle.js
@@ -0,0 +1,10 @@
+//@ts-check
+
+import { Style } from "../viewer/Style"
+import { Cell } from "../viewer/Dataset"
+import { CanvasGeo } from "../viewer/CanvasGeo";
+
+export class FlagStyle extends Style {
+
+
+}
