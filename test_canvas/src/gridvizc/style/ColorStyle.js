7aebcd5b0bc0c43d53417a93be82f1a197ef7b99
jgaffuri <jgaf@protonmail.com>
2021-11-22 14:29:09 +0100

    test tiling with canvas

--- test_canvas/src/gridvizc/style/ColorStyle.js
+++ test_canvas/src/gridvizc/style/ColorStyle.js
@@ -16,20 +16,20 @@ export class ColorStyle extends Style {
 
 
     /**
-     * Draw cells.
+     * Draw cells as squares depending on their value.
      * 
      * @param {Array.<Cell>} cells 
      * @param {number} resolution 
      * @param {CanvasGeo} cg 
      */
     draw(cells, resolution, cg) {
-
-        console.log(cells.length)
-
         const c2 = cg.c2d
         for (let cell of cells) {
+            //get value
             const value = this.getValue(cell);
+            //get color
             c2.fillStyle = this.getColor(value);
+            //draw square
             c2.fillRect(cg.geoToPixX(cell.x), cg.geoToPixY(cell.y), resolution / cg.ps, resolution / cg.ps);
         }
     }
