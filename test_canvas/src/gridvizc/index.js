557ad1afa7807abd0c6fa422ffae5575591d9d0b
jgaffuri <jgaf@protonmail.com>
2021-11-23 21:36:49 +0100

    test tiling with canvas

--- test_canvas/src/gridvizc/index.js
+++ test_canvas/src/gridvizc/index.js
@@ -1,7 +1,13 @@
 //@ts-check
 
 import { GridVizCanvas } from "./viewer/GridVizCanvas"
+import * as dsc from "d3-scale-chromatic"
 
 export const gridvizApp = function (opts) {
     return new GridVizCanvas(opts)
+}
+
+//export all d3 things
+export const color = function () {
+    return dsc
 }
