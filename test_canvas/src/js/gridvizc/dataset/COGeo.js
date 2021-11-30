3552cf1e66117eca85e6154f45d383c1099f8ef9
jgaffuri <jgaf@protonmail.com>
2021-11-30 16:00:02 +0100

    test canvas

--- test_canvas/src/js/gridvizc/dataset/COGeo.js
+++ test_canvas/src/js/gridvizc/dataset/COGeo.js
@@ -1,4 +1,19 @@
 //@ts-check
+import { Dataset, Cell, Envelope } from "../Dataset"
 
+/**
+ * A dataset composed of a COGeo file.
+ * @see https://www.cogeo.org/
+ * 
+ * @author Julien Gaffuri
+ */
+export class GeoTiff extends Dataset {
 
-//TODO https://www.cogeo.org/
+     constructor(url, resolution, preprocess = null) {
+        super(url, resolution, preprocess)
+
+    }
+
+//TODO Use https://geotiffjs.github.io/
+
+}
--- test_canvas/src/js/gridvizc/dataset/GeoTiff.js
+++ test_canvas/src/js/gridvizc/dataset/GeoTiff.js
@@ -1,4 +1,18 @@
 //@ts-check
+import { Dataset, Cell, Envelope } from "../Dataset"
 
-//TODO geotiff format. Use https://geotiffjs.github.io/
+/**
+ * A dataset composed of a single GeoTiff file.
+ * 
+ * @author Julien Gaffuri
+ */
+export class GeoTiff extends Dataset {
 
+     constructor(url, resolution, preprocess = null) {
+        super(url, resolution, preprocess)
+
+    }
+
+//TODO Use https://geotiffjs.github.io/
+
+}
