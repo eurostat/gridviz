3a5a15b6f93fc5dd00e86b1d4216d3786e31479c
jgaffuri <jgaf@protonmail.com>
2021-12-03 13:57:39 +0100

    test canvas

--- test_canvas/src/js/gridvizc/style/TextStyle.js
+++ test_canvas/src/js/gridvizc/style/TextStyle.js
@@ -14,9 +14,9 @@ export class TextStyle extends Style {
       * @param {function(Cell):string} text A function returning the text of a cell.
       * @param {function(Cell):string} color A function returning the color of the cell.
       * @param {function(Cell):number} fontSize A function returning the font size of the cell label.
-      * @param {function(Cell):string} fontFamily A function returning the font family of the cell label.
+      * @param {function(Cell):string} font A function returning the font info of the cell label, other than the size. Ex: "italic small-caps bold arial"
       */
-     constructor(text, color = () => "black", fontSize = () => 10, fontFamily = () => "Arial") {
+    constructor(text, color = () => "black", fontSize = () => 10, font = () => "bold Arial") {
         super()
 
         /** @private @type {function(Cell):string} */
@@ -30,9 +30,9 @@ export class TextStyle extends Style {
         this.fontSize = fontSize;
         //TODO fontsize in geo also ? Here it is in pix only.
 
-        /** @private @type {function(Cell):string} */
-        this.fontFamily = fontFamily;
-        //TODO add bold/regular/italic ? maybe covered by font family
+        /** Example: "italic small-caps bold arial"
+         * @private @type {function(Cell):string} */
+        this.font = font;
     }
 
 
@@ -53,26 +53,26 @@ export class TextStyle extends Style {
 
             //get cell text
             const text = this.text(cell);
-            if(!text) continue;
+            if (!text) continue;
 
             //text color
             cg.ctx.fillStyle = this.color ? this.color(cell) : "#EA6BAC";
 
             //text size and font
-            const fontSize = this.fontSize? this.fontSize(cell) : "10";
-            const fontFamily = this.fontFamily? this.fontFamily(cell) : "Arial";
+            const fontSize = this.fontSize ? this.fontSize(cell) : 10;
+            const fontFamily = this.fontFamily ? this.fontFamily(cell) : "Arial";
             cg.ctx.font = fontSize + "px " + fontFamily;
 
             //text position
             cg.ctx.textAlign = "center";
             const tx = cg.geoToPixX(cell.x + r * 0.5 + this.offset.dx);
-            const ty = cg.geoToPixY(cell.y + r * 0.5 + this.offset.dy);
+            const ty = cg.geoToPixY(cell.y + r * 0.5 + this.offset.dy) + fontSize * 0.5;
 
             //draw the text
             cg.ctx.fillText(text, tx, ty);
 
             //draw stroke of the cell as a square ?
-            //this.drawStroke(cell, r, cg, c => "square")
+            this.drawStroke(cell, r, cg, c => "square")
         }
 
     }
