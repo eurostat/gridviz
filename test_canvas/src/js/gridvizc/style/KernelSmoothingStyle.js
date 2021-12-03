33422bde5273346bcc4f47eb566e9ab31872975f
jgaffuri <jgaf@protonmail.com>
2021-12-03 10:58:40 +0100

    test canvas

---
+++ test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
@@ -0,0 +1,10 @@
+
+//See:
+
+//https://github.com/Planeshifter/kernel-smooth/blob/master/examples/index.js
+//https://gist.github.com/curran/b595fde4d771c5784421
+//https://github.com/jasondavies/science.js/tree/master/examples/kde
+
+//https://bl.ocks.org/rpgove/210f679b1087b517ce654b717e8247ac
+//https://observablehq.com/@d3/kernel-density-estimation
+
