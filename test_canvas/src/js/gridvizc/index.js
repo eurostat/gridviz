f579bb7468ed5d6369444811b0a89e6b25be2a16
jgaffuri <jgaf@protonmail.com>
2021-12-04 13:36:28 +0100

    test canvas

--- test_canvas/src/js/gridvizc/index.js
+++ test_canvas/src/js/gridvizc/index.js
@@ -4,12 +4,7 @@
 export { App } from "./App"
 
 //export color (the entire d3 scale chromatic) //TODO simpler ?
-//export * as color2 from "d3-scale-chromatic"
-import * as dsc from "d3-scale-chromatic"
-export const color = function () {
-    return dsc
-}
-
+export * from "d3-scale-chromatic"
 
 //export dataset types
 export { CSVGrid } from "./dataset/CSVGrid"
