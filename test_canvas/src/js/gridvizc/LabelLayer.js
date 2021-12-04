a158e2d2351b0a88ffd23f4a6f316c0587b30c4f
jgaffuri <jgaf@protonmail.com>
2021-12-04 10:55:37 +0100

    test canvas

--- test_canvas/src/js/gridvizc/LabelLayer.js
+++ test_canvas/src/js/gridvizc/LabelLayer.js
@@ -4,68 +4,54 @@ import { csv } from "d3-fetch";
 import { geoAzimuthalEqualArea } from 'd3-geo'
 import { CanvasGeo } from "./CanvasGeo";
 
+/** @typedef {{name: string, cat: number, pop_2011:number, lon:number, lat:number, x:number, y:number }} Label */
+
+
 /**
+ * A layer specifying labels.
+ * The input is a CSV file with the position of the labels and name + some other info on the label importance.
+ * If the label position is not in the same CRS as the grid, it can be reporjected with the "projection" function.
+ * The selection of the label, their style (font, weight, etc.) and color can be specified depending on their importance and the zoom level.
  * 
  * @author Julien Gaffuri
  */
 export class LabelLayer {
 
     /**
+     * 
+     * @param {string} url The URL of the label data, as CSV file
+     * @param {function(Label,number):string} labelStyle Specify if and how a lable should be drawn, depending on its importance and the zoom level.
+     * @param {function(Label,number):string} labelColor Specify the label color, depending on its importance and the zoom level.
+     * @param {function} projection 
      */
-    constructor(url, labelStyle, projection) {
+    constructor(url, labelStyle = lb=>"bold 15px Arial", labelColor = lb=>"#00000044", projection = (a=>a)) {
 
         /** @type {string} */
-        this.url = url || "https://raw.githubusercontent.com/eurostat/gridviz/master/assets/csv/names.csv"
+        this.url = url
 
-        /** @typedef {{name: string, cat: number, pop_2011:number, lon:number, lat:number, x:number, y:number }} Label */
-        /** @type {Array.<Label>} */
-        this.labels = undefined;
+        /** Return label style depending on its importance and the zoom level
+         * @type {function(Label,number):string} */
+        this.labelStyle = labelStyle
+
+        /** Return label color depending on its importance and the zoom level
+         * @type {function(Label,number):string} */
+        this.labelColor = labelColor
 
         /** The projection from (lat,lon) to the CRS of the grid.
           * ITt is used for example to project and show the labels in the foreground.
           * By default, it is set to European projection, ETRS89-LAEA (EPSG:3035)
           * @type {function} */
-        this.projection = projection || geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000])//.scale(1)
+         this.projection = projection
 
-        //labels selection function
-
-        /** Return label style depending on its importance and the zoom level
-         * @type {function(Label,number):string} */
-        this.labelStyle = labelStyle || function(tn, zf) {
-            if (zf < 50) {
-                return "bold 30px Arial";
-            }
-            if (zf < 100) {
-                return "bold 20px Arial";
-            }
-            if (zf < 200) {
-                return "bold 15px Arial";
-            }
-            if (zf < 300) {
-                if (tn.cat == 2) return
-                return "bold 15px Arial";
-            }
-            if (zf < 400) {
-                if (tn.cat == 2) return
-                if (tn.pop_2011 < 10000) return
-                return "bold 15px Arial";
-            }
-            if (zf < 500) {
-                if (tn.cat == 2) return
-                if (tn.pop_2011 < 50000) return
-                return "bold 15px Arial";
-            }
-
-            if (tn.cat == 2) return
-            if (tn.pop_2011 < 500000) return
-            return "bold 15px Arial";
-        }
+        /** @private @type {Array.<Label>} */
+        this.labels = undefined
 
     }
 
 
     /**
      * Draw the label layer.
+     * 
      * @param {CanvasGeo} cg
      * @returns {void}
      */
@@ -77,21 +63,23 @@ export class LabelLayer {
             return;
         }
 
-        cg.ctx.fillStyle = "#00000044";
         cg.ctx.textAlign = "center";
-        for (const tn of this.labels) {
+        for (const lb of this.labels) {
+
+            //set color
+            cg.ctx.fillStyle = this.labelColor(lb, cg.zf);
 
             //get label style
-            const st = this.labelStyle(tn, cg.zf);
+            const st = this.labelStyle(lb, cg.zf);
             if (!st) continue;
             cg.ctx.font = st;
 
             //get label position
-            const tx = cg.geoToPixX(tn.x);
-            const ty = cg.geoToPixY(tn.y);
+            const tx = cg.geoToPixX(lb.x);
+            const ty = cg.geoToPixY(lb.y);
 
             //draw label
-            cg.ctx.fillText(tn.name, tx, ty);
+            cg.ctx.fillText(lb.name, tx, ty);
         }
     }
 
@@ -101,6 +89,13 @@ export class LabelLayer {
      * @private
      */
     load(callback) {
+
+        if(!this.url) {
+            console.log("Failed loading labels. No URL specified." + this.url)
+            this.labels = []
+            return;
+        }
+
         csv(this.url)
             .then(
                 /** @param {*} data */
@@ -108,10 +103,10 @@ export class LabelLayer {
                     this.labels = data;
 
                     //project labels
-                    for (const tn of this.labels) {
-                        const p = this.projection([tn.lon, tn.lat])
-                        tn.x = p[0]; tn.y = p[1];
-                        delete tn.lon; delete tn.lat;
+                    for (const lb of this.labels) {
+                        const p = this.projection([lb.lon, lb.lat])
+                        lb.x = p[0]; lb.y = p[1];
+                        delete lb.lon; delete lb.lat;
                     }
 
                     //redraw
@@ -123,4 +118,51 @@ export class LabelLayer {
             });
     }
 
+}
+
+
+/**
+ * Returns label layer from Eurostat, in LAEA projection
+ * @returns {LabelLayer}
+ */
+export const getEurostatLabelLayer = function(){
+    const lay = new LabelLayer(
+        //url
+        "https://raw.githubusercontent.com/eurostat/gridviz/master/assets/csv/names.csv",
+        //style
+        function(lb, zf) {
+            if (zf < 50) {
+                return "bold 30px Arial";
+            }
+            if (zf < 100) {
+                return "bold 20px Arial";
+            }
+            if (zf < 200) {
+                return "bold 15px Arial";
+            }
+            if (zf < 300) {
+                if (lb.cat == 2) return
+                return "bold 15px Arial";
+            }
+            if (zf < 400) {
+                if (lb.cat == 2) return
+                if (lb.pop_2011 < 10000) return
+                return "bold 15px Arial";
+            }
+            if (zf < 500) {
+                if (lb.cat == 2) return
+                if (lb.pop_2011 < 50000) return
+                return "bold 15px Arial";
+            }
+
+            if (lb.cat == 2) return
+            if (lb.pop_2011 < 500000) return
+            return "bold 15px Arial";
+        },
+        //color
+        lb=>"#00000044",
+        //projection
+        geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000]),
+    )
+    return lay;
 }
