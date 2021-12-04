f08176cb562f160e347180bf3320da3487c1050e
jgaffuri <jgaf@protonmail.com>
2021-12-04 13:43:16 +0100

    test canvas

--- test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
+++ test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
@@ -11,17 +11,9 @@ import { CanvasGeo } from "../CanvasGeo";
  */
 export class KernelSmoothingStyle extends Style {
 
-    //See:
-//https://github.com/Planeshifter/kernel-smooth/blob/master/examples/index.js
-//https://gist.github.com/curran/b595fde4d771c5784421
-//https://github.com/jasondavies/science.js/tree/master/examples/kde
-
-//https://bl.ocks.org/rpgove/210f679b1087b517ce654b717e8247ac
-//https://observablehq.com/@d3/kernel-density-estimation
-
-     constructor() {
+    constructor() {
         super()
-     }
+    }
 
     /**
      * Draw cells as squares depending on their value.
@@ -30,9 +22,19 @@ export class KernelSmoothingStyle extends Style {
      * @param {number} r 
      * @param {CanvasGeo} cg 
      */
-     draw(cells, r, cg) {
-         //
-     }
+    draw(cells, r, cg) {
+
+        //TODO
+        //See:
+        //https://github.com/Planeshifter/kernel-smooth/blob/master/examples/index.js
+        //https://gist.github.com/curran/b595fde4d771c5784421
+        //https://github.com/jasondavies/science.js/tree/master/examples/kde
+
+        //https://bl.ocks.org/rpgove/210f679b1087b517ce654b717e8247ac
+        //https://observablehq.com/@d3/kernel-density-estimation
+
+        
+    }
 
 
     //getters and setters
