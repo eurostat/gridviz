373afef90aa48a3e048335275da28431889aed35
jgaffuri <jgaf@protonmail.com>
2021-12-04 13:40:48 +0100

    test canvas

--- test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
+++ test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
@@ -1,6 +1,17 @@
 
-//See:
+//@ts-check
 
+import { Style } from "../Style"
+import { Cell } from "../Dataset"
+import { CanvasGeo } from "../CanvasGeo";
+
+/**
+ * 
+ * @author Julien Gaffuri
+ */
+export class KernelSmoothingStyle extends Style {
+
+    //See:
 //https://github.com/Planeshifter/kernel-smooth/blob/master/examples/index.js
 //https://gist.github.com/curran/b595fde4d771c5784421
 //https://github.com/jasondavies/science.js/tree/master/examples/kde
@@ -8,3 +19,23 @@
 //https://bl.ocks.org/rpgove/210f679b1087b517ce654b717e8247ac
 //https://observablehq.com/@d3/kernel-density-estimation
 
+     constructor() {
+        super()
+     }
+
+    /**
+     * Draw cells as squares depending on their value.
+     * 
+     * @param {Array.<Cell>} cells 
+     * @param {number} r 
+     * @param {CanvasGeo} cg 
+     */
+     draw(cells, r, cg) {
+         //
+     }
+
+
+    //getters and setters
+    //TODO
+
+}
