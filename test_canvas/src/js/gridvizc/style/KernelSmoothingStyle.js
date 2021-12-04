402875cdb57477941866160be23e28655faebdaa
jgaffuri <jgaf@protonmail.com>
2021-12-04 22:42:32 +0100

    test canvas

--- test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
+++ test_canvas/src/js/gridvizc/style/KernelSmoothingStyle.js
@@ -55,17 +55,8 @@ export class KernelSmoothingStyle extends Style {
         const nbY = (yMax - yMin) / r + 1
         console.log(nbX, nbY)
 
-        //create input matrix (with 0 as values)
-        let matrix = [];
-        for (let i = 0; i < nbX; i++) {
-            const col = [];
-            for (let j = 0; j < nbY; j++) {
-                col.push(0);
-            }
-            matrix.push(col);
-        }
-
-        //fill input matrix with input figures, not smoothed
+        //create and fill input matrix with input figures, not smoothed
+        let matrix = getEmptyMatrix(nbX, nbY);
         for (const c of cells) {
             if (c.x < xMin || c.x > xMax || c.y < yMin || c.y >= yMax)
                 continue;
@@ -76,6 +67,8 @@ export class KernelSmoothingStyle extends Style {
 
         //compute smoothed matrix
         matrix = kernelSmoothing(matrix, nbX, nbY, this.sigmaGeo / r)
+
+        console.log(matrix)
 
         //draw smoothed matrix
         //TODO
@@ -90,14 +83,70 @@ export class KernelSmoothingStyle extends Style {
 
 }
 
+
+/**
+ * Create a matrix full of zeros.
+ * 
+ * @param {number} nbX 
+ * @param {number} nbY 
+ * @returns 
+ */
+function getEmptyMatrix(nbX, nbY) {
+    const matrix = []
+    for (let i = 0; i < nbX; i++) {
+        const col = [];
+        for (let j = 0; j < nbY; j++) {
+            col.push(0);
+        }
+        matrix.push(col);
+    }
+    return matrix;
+}
+
+
+/**
+ * Compute kernel smoothing.
+ * 
+ * @param {Array.<Array.<number>>} m The input matrix to be smoothed
+ * @param {number} nbX Size of the input matrix - X
+ * @param {number} nbY Size of the input matrix - Y
+ * @param {number} sigma Smoothing std deviation, in number of cells
+ * @returns {Array.<Array.<number>>}
+ */
 function kernelSmoothing(m, nbX, nbY, sigma) {
 
+    //create output matrix
+    const out = getEmptyMatrix(nbX, nbY);
+
     //prepare coefficients for gaussian computation
+    //to avoid computing them every time.
     const c1 = sigma * Math.sqrt(2 * Math.PI);
     const c2 = 2 * sigma * sigma;
 
     //the gaussian function.
     const gaussian = (x) => Math.exp(-x * x / c2) / c1
 
-    return m;
+    //the size of the window: lets limit that to 5 times the standard deviation, as an approximation.
+    const windowSize = Math.floor(5 * sigma) + 1;
+
+    //make smoothing, cell by cell
+    for (let i = 0; i < nbX; i++) {
+        for (let j = 0; j < nbY; j++) {
+            //compute smoothed value, at i,j
+            let sval = 0;
+            let sumWeights = 0;
+            for (let wi = -windowSize; wi <= windowSize; wi++)
+                for (let wj = -windowSize; wj <= windowSize; wj++) {
+                    //compute weight of pixel (i+wi,j+wj)
+                    const weight = gaussian( Math.sqrt(wi*wi+wj*wj) ); //TODO with gaussian
+                    //add contribution of pixel (i+wi,j+wj): its weight times its value
+                    sval += weight * m[i+wi][j+wj]
+                    //keep sum of weights
+                    sumWeights += weight;
+                }
+            //TODO check sumWeights is (almost) equal to 1
+            out[i][j] = sval / sumWeights
+        }
+    }
+    return out;
 }
