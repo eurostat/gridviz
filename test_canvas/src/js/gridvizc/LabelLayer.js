c5cd6c2f2537786fe99eefeab82bef1c9aae2674
jgaffuri <jgaf@protonmail.com>
2021-12-06 15:24:39 +0100

    test canvas

--- test_canvas/src/js/gridvizc/LabelLayer.js
+++ test_canvas/src/js/gridvizc/LabelLayer.js
@@ -20,7 +20,7 @@ export class LabelLayer {
      * @param {string} url The URL of the label data, as CSV file
      * @param {function(Label,number):string} labelStyle Specify if and how a lable should be drawn, depending on its importance and the zoom level.
      * @param {function(Label,number):string} labelColor Specify the label color, depending on its importance and the zoom level.
-     * @param {function(Label):void} preprocess A preprocess to run on each label after loading. It can be used to apply some specific treatment before, format the label data, project coordinates, etc.
+     * @param {function(object):void} preprocess A preprocess to run on each label after loading. It can be used to apply some specific treatment before, format the label data, project coordinates, etc.
      */
     constructor(url, labelStyle = lb => "bold 15px Arial", labelColor = lb => "#00000044", preprocess = null) {
 
@@ -35,7 +35,7 @@ export class LabelLayer {
          * @private @type {function(Label,number):string} */
         this.labelColor = labelColor
 
-        /** @private @type {function(Label):void} */
+        /** @private @type {function(object):void} */
         this.preprocess = preprocess;
 
         /** @private @type {Array.<Label>} */
@@ -94,12 +94,13 @@ export class LabelLayer {
             .then(
                 /** @param {*} data */
                 (data) => {
-                    this.labels = data;
-
                     //apply preprocess
                     if (this.preprocess)
-                        for (const lb of this.labels)
+                        for (const lb of data)
                             this.preprocess(lb)
+
+                    //store labels
+                    this.labels = data;
 
                     //redraw
                     if (callback) callback()
@@ -129,9 +130,9 @@ export class LabelLayer {
     /** @param {function(Label,number):string} val @returns {this} */
     setLabelColor(val) { this.labelColor = val; return this; }
 
-    /** @returns {function(Label):void} */
+    /** @returns {function(object):void} */
     getPreprocess() { return this.preprocess; }
-    /** @param {function(Label):void} val @returns {this} */
+    /** @param {function(object):void} val @returns {this} */
     setPreprocess(val) { this.preprocess = val; return this; }
 
 }
@@ -143,7 +144,8 @@ export class LabelLayer {
  */
 export const getEurostatLabelLayer = function () {
 
-    console.log(geoAzimuthalEqualArea())
+    //ETRS89-LAEA projection
+    const proj = geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000]);
 
     return new LabelLayer(
         //url
@@ -160,36 +162,30 @@ export const getEurostatLabelLayer = function () {
                 return "bold 15px Arial";
             }
             if (zf < 300) {
-                if (lb.cat == 2) return
+                if (lb["cat"] == 2) return
                 return "bold 15px Arial";
             }
             if (zf < 400) {
-                if (lb.cat == 2) return
-                if (lb.pop_2011 < 10000) return
+                if (lb["cat"] == 2) return
+                if (lb["pop_2011"] < 10000) return
                 return "bold 15px Arial";
             }
             if (zf < 500) {
-                if (lb.cat == 2) return
-                if (lb.pop_2011 < 50000) return
+                if (lb["cat"] == 2) return
+                if (lb["pop_2011"] < 50000) return
                 return "bold 15px Arial";
             }
 
-            if (lb.cat == 2) return
-            if (lb.pop_2011 < 500000) return
+            if (lb["cat"] == 2) return
+            if (lb["pop_2011"] < 500000) return
             return "bold 15px Arial";
         },
         //color
         lb => "#00000044",
-        //projection
-        // @ts-ignore
-        geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000]),
+        lb => {
+            const p = this.projection([lb.lon, lb.lat])
+            lb.x = p[0]; lb.y = p[1];
+            delete lb.lon; delete lb.lat;
+        }
     )
 }
-
-
-
-                    /*{
-                        const p = this.projection([lb.lon, lb.lat])
-                        lb.x = p[0]; lb.y = p[1];
-                        delete lb.lon; delete lb.lat;
-                    }*/
