6336c77cf892d47b3486236a0d8425816a31022d
jgaffuri <jgaf@protonmail.com>
2021-12-06 17:41:13 +0100

    test canvas

--- test_canvas/src/js/gridvizc/style/RadarStyle.js
+++ test_canvas/src/js/gridvizc/style/RadarStyle.js
@@ -4,9 +4,9 @@ import { Style, Size } from "../Style"
 import { Cell } from "../Dataset"
 import { CanvasGeo } from "../CanvasGeo";
 
-/** @typedef {"square"|"circle"} Shape */
-
 /**
+ * A style showing the composition of the grid cell as a "radar" chart.
+ * It is a bit like a pie chart, except the angular amplitude of each part is the same but the radius of the part changes depending on the category importance.
  * 
  * @author Julien Gaffuri
  */
@@ -19,10 +19,13 @@ export class RadarStyle extends Style {
 
         /**
          * The dictionary which give the color of each category.
+         * 
          * @private @type {object} */
         this.color = opts.color;
 
         /**
+         * The function specifying how the radius evolves depending on the statistical value.
+         * 
          * @private @type {{val: function(number):number, unit: "pix"|"geo"}} */
         this.radius = opts.radius;
 
@@ -30,7 +33,7 @@ export class RadarStyle extends Style {
 
 
     /**
-     * Draw cells as squares depending on their value.
+     * Draw cells.
      * 
      * @param {Array.<Cell>} cells 
      * @param {number} resolution 
@@ -40,8 +43,10 @@ export class RadarStyle extends Style {
 
         //nb categories
         const nbCat = Object.entries(this.color).length
+
+        //angle for each category: the same for all.
         const angle = 2 * Math.PI / nbCat
-        let angleCumul = 0
+        let angleCumul = Math.PI
 
         for (let cell of cells) {
 
@@ -49,33 +54,45 @@ export class RadarStyle extends Style {
             const xc = cg.geoToPixX(cell.x + resolution * 0.5 + this.offset.dx);
             const yc = cg.geoToPixY(cell.y + resolution * 0.5 + this.offset.dy);
 
-            //draw decomposition symbol
+            //draw decomposition symbols
             for (let [column, color] of Object.entries(this.color)) {
 
-                //set color
+                //set category color
                 cg.ctx.fillStyle = color;
 
                 //get categroy value
                 const val = cell[column]
 
-                //radius - in pixel
+                //compute category radius - in pixel
                 /** @type {number} */
                 const rP = this.radius.unit === "pix" ? this.radius.val(val) : this.radius.val(val) / cg.zf
 
+                //draw angular sector
                 cg.ctx.beginPath();
                 cg.ctx.moveTo(xc, yc);
-                cg.ctx.arc(xc, yc, rP, angleCumul, angleCumul + angle);
+                cg.ctx.arc(xc, yc, rP, angleCumul -angle , angleCumul);
                 cg.ctx.lineTo(xc, yc);
                 cg.ctx.fill();
 
-                //
-                angleCumul += angle
-
-                //TODO draw stroke ?
+                //next angular sector
+                angleCumul -= angle
             }
 
         }
 
     }
+
+
+    //getters and setters
+
+    /** @returns {function(Cell):string} */
+    getColor() { return this.color; }
+    /** @param {function(Cell):string} val @returns {this} */
+    setColor(val) { this.color = val; return this; }
+
+    /** @returns {{val: function(number):number, unit: "pix"|"geo"}} */
+    getRadius() { return this.radius; }
+    /** @param {{val: function(number):number, unit: "pix"|"geo"}} val @returns {this} */
+    setRadius(val) { this.radius = val; return this; }
 
 }
