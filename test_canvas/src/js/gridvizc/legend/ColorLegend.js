1959bdb9b09526356004259bed67cc4c71f2abe3
jgaffuri <jgaf@protonmail.com>
2022-06-08 11:25:19 +0200

    a

---
+++ test_canvas/src/js/gridvizc/legend/ColorLegend.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+import { Style, Stat, getStatistics } from "../Style"
+import { Legend } from "../Legend";
+
+
+/**
+ * @author Julien Gaffuri
+ */
+export class ColorLegend extends Legend {
+
+    constructor(id, opts) {
+        super(id, opts)
+
+        const svg = this.div.append("svg")//.attr("width",50).attr("height",100)
+        svg.append("rect").attr("x",0).attr("y",0).attr("width",10).attr("height",100).style("fill", "red")
+//  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+
+    }
+
+}
