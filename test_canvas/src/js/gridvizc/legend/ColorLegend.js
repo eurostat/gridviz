2ee44455ce5d929970e54512f835612158c31d55
jgaffuri <jgaf@protonmail.com>
2022-06-08 16:37:44 +0200

    a

--- test_canvas/src/js/gridvizc/legend/ColorLegend.js
+++ test_canvas/src/js/gridvizc/legend/ColorLegend.js
@@ -18,6 +18,8 @@ export class ColorLegend extends Legend {
     constructor(style, opts) {
         super(style, opts)
 
+        //needed: Function (t) -> value
+
         const svg = this.div.append("svg")//.attr("width",50).attr("height",100)
         svg.append("rect").attr("x",0).attr("y",0).attr("width",10).attr("height",100).style("fill", "red")
 //  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
