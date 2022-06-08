353b8108d8eb3771b84545ab2586e5ca07d26924
jgaffuri <jgaf@protonmail.com>
2022-06-08 10:05:24 +0200

    a

---
+++ test_canvas/src/js/ColorLegend.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+import { select } from "d3-selection";
+import { zoom, zoomIdentity } from "d3-zoom";
+
+/**
+ * 
+ * @author Julien Gaffuri
+ */
+export class ColorLegend {
+
+    constructor(canvasId = "vacanvas", center = undefined, zf = 1) {
+    }
+
+}
