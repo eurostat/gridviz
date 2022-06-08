d9510d386a1a1703f66adddde47a6326c12f2dd3
jgaffuri <jgaf@protonmail.com>
2022-06-08 21:14:44 +0200

    a

--- test_canvas/src/js/gridvizc/legend/ColorLegend.js
+++ test_canvas/src/js/gridvizc/legend/ColorLegend.js
@@ -19,7 +19,7 @@ export class ColorLegend extends Legend {
     }
 
     /**
-     * @param {*} opts 
+     * @param {object} opts 
      */
     update(opts) {
         //needed: function (t) -> value  +  color ramp
@@ -27,14 +27,15 @@ export class ColorLegend extends Legend {
         //clear
         this.div.selectAll("*").remove();
 
-
-        console.log(this.colorRamp)
-        console.log(this.fun)
-
         const svg = this.div.append("svg")//.attr("width",50).attr("height",100)
-        svg.append("rect").attr("x", 0).attr("y", 0).attr("width", 10).attr("height", 100).style("fill", "red")
         //  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
 
+        const w = 15
+        const h = 100
+        for (let i = 0; i < h; i++) {
+            svg.append("rect").attr("x", 0).attr("y", i).attr("width", w).attr("height", 1).style("fill", this.colorRamp(i / (h - 1)))
+        }
+        svg.append("rect").attr("x", 0).attr("y", 0).attr("width", w).attr("height", h).style("fill", "none").style("stroke", "lightgray")
     }
 
 }
