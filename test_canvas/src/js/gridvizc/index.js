fb55bd12b8a898caef1bf0f2b0ea234120701980
jgaffuri <jgaf@protonmail.com>
2022-06-14 16:13:22 +0200

    a

--- test_canvas/src/js/gridvizc/index.js
+++ test_canvas/src/js/gridvizc/index.js
@@ -106,7 +106,7 @@ export const getEurostatLabelLayer = function () {
     const proj = geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000]);
 
     return new LabelLayer(
-        "https://raw.githubusercontent.com/eurostat/euronym/main/pub/v1/EUR.csv",
+        "https://raw.githubusercontent.com/eurostat/euronym/main/pub/v1/50/EUR.csv",
         {
             style: (lb, zf) => {
                 if(lb.rmax < zf) return;
