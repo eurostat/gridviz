8f30f8cf26bc44ff705b730710ab362f3d3abfef
jgaffuri <jgaf@protonmail.com>
2022-06-15 20:23:09 +0200

    a

--- test_canvas/src/js/gridvizc/index.js
+++ test_canvas/src/js/gridvizc/index.js
@@ -19,7 +19,7 @@ export { TextStyle } from "./style/TextStyle"
 export { KernelSmoothingStyle } from "./style/KernelSmoothingStyle"
 export { RadarStyle } from "./style/RadarStyle"
 export { AgePyramidStyle } from "./style/AgePyramidStyle"
-export { LineUpStyle } from "./style/LinesUpStyle"
+export { LineUpStyle } from "./style/LineUpStyle"
 
 //export label layer
 export { LabelLayer } from "./LabelLayer"
@@ -33,64 +33,6 @@ export { LabelLayer } from "./LabelLayer"
 
 import { geoAzimuthalEqualArea } from 'd3-geo'
 import { LabelLayer } from "./LabelLayer"
-
-
-
-/**
- * Returns label layer from Eurostat, for ETRS89-LAEA grids.
- * From dataset: https://raw.githubusercontent.com/eurostat/gridviz/master/assets/csv/names.csv
- * 
- * @returns {LabelLayer}
- */
-export const getEurostatLabelLayer = function () {
-
-    //ETRS89-LAEA projection
-    const proj = geoAzimuthalEqualArea().rotate([-10, -52]).reflectX(false).reflectY(true).scale(6378137).translate([4321000, 3210000]);
-
-    return new LabelLayer(
-        "https://raw.githubusercontent.com/eurostat/gridviz/master/assets/csv/names.csv",
-        {
-            style: (lb, zf) => {
-                if (zf < 50) {
-                    return "bold 30px Arial";
-                }
-                if (zf < 100) {
-                    return "bold 20px Arial";
-                }
-                if (zf < 200) {
-                    return "bold 15px Arial";
-                }
-                if (zf < 300) {
-                    if (lb["cat"] == 2) return
-                    return "bold 15px Arial";
-                }
-                if (zf < 400) {
-                    if (lb["cat"] == 2) return
-                    if (lb["pop_2011"] < 10000) return
-                    return "bold 15px Arial";
-                }
-
-                if (lb["cat"] == 2) return
-                if (lb["pop_2011"] < 400000) return
-                return "bold 15px Arial";
-            },
-            //color
-            color: () => "#00000044",
-            //halo color
-            haloColor: () => null,
-            //halo width
-            haloWidth: () => 0,
-            //preprocess
-            preprocess: lb => {
-                //project from geo coordinates to ETRS89-LAEA
-                const p = proj([lb.lon, lb.lat])
-                lb.x = p[0]; lb.y = p[1];
-                delete lb.lon; delete lb.lat;
-            }
-        }
-    )
-}
-
 
 
 
