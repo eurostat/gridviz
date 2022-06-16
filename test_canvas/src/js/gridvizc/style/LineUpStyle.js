c30075fe6eb21bf6b69de54ca97c7836cf215aec
jgaffuri <jgaf@protonmail.com>
2022-06-16 16:25:45 +0200

    a

--- test_canvas/src/js/gridvizc/style/LineUpStyle.js
+++ test_canvas/src/js/gridvizc/style/LineUpStyle.js
@@ -24,13 +24,18 @@ export class LineUpStyle extends Style {
         this.colorCol = opts.colorCol;
         /** A function returning the color of the line representing a cell.
         * @private @type {function(number,number,Stat):string} */
-        this.color = opts.color || (() => "#EA6BAC");
+        this.color = opts.color || (() => "#EA6BACAA");
 
         /** @private @type {string} */
         this.widthCol = opts.widthCol;
         /** A function returning the width of the line representing a cell, in geo unit
          * @private @type {function(number,number,Stat,number):number} */
-        this.width = opts.width || ((v, r) => 0.1 * r);
+        this.width = opts.width || ((v, r) => 0.2 * r);
+
+        this.shadowDirection = 210 * Math.PI / 180.0;
+        this.shadowFactor = 0.5;
+        this.shadowColor = "#CCC";
+
     }
 
 
@@ -70,6 +75,43 @@ export class LineUpStyle extends Style {
         const H = 1000 * cg.getZf()
 
         cg.ctx.lineCap = "round";
+
+        //shadows
+        cg.ctx.strokeStyle = this.shadowColor
+        for (let c of cells) {
+
+            //width
+            /** @type {number} */
+            const wG = this.width ? this.width(c[this.widthCol], resolution, statWidth, cg.zf) : undefined
+            if (!wG || wG < 0) continue
+
+            //height
+            /** @type {number} */
+            const hG = this.height ? this.height(c[this.heightCol], resolution, statHeight, cg.zf) : undefined
+            if (!hG || hG < 0) continue
+
+            //get offset
+            //TODO use that
+            const offset = this.offset(c, resolution, cg.zf)
+
+            //set width
+            cg.ctx.lineWidth = wG / cg.zf
+
+            //compute cell centre postition
+            const cx = c.x + resolution / 2;
+            const cy = c.y + resolution / 2;
+            const ls = hG * this.shadowFactor
+
+            //draw segment
+            cg.ctx.beginPath();
+            cg.ctx.moveTo(cg.geoToPixX(cx), cg.geoToPixY(cy));
+            cg.ctx.lineTo(cg.geoToPixX(cx + ls * Math.cos(this.shadowDirection)), cg.geoToPixY(cy + ls * Math.sin(this.shadowDirection)));
+            cg.ctx.stroke();
+
+        }
+
+
+
         for (let c of cells) {
 
             //color
