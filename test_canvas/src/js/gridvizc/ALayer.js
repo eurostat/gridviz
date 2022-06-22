a1e6693c27c72fb7b7e1f78081a090235741bbfa
jgaffuri <jgaf@protonmail.com>
2022-06-22 20:49:08 +0200

    a

---
+++ test_canvas/src/js/gridvizc/ALayer.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+import { Dataset } from "./Dataset";
+import { Style } from "./Style";
+
+/**
+ * 
+ * @author Julien Gaffuri
+ */
+export class ALayer {
+
+    constructor() {
+    }
+
+}
---
+++ test_canvas/src/js/gridvizc/MultiScaleLayer.js
@@ -0,0 +1,17 @@
+//@ts-check
+
+import { ALayer } from "./ALayer";
+import { Dataset } from "./Dataset";
+import { Style } from "./Style";
+
+/**
+ * 
+ * @author Julien Gaffuri
+ */
+export class MultiScaleLayer extends ALayer {
+
+    constructor() {
+        super()
+    }
+
+}
