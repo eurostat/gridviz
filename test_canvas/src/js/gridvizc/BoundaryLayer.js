2252769c9f11e7c651043f038e246ce0c8dd16d4
jgaffuri <jgaf@protonmail.com>
2022-06-22 11:30:52 +0200

    a

--- test_canvas/src/js/gridvizc/BoundaryLayer.js
+++ test_canvas/src/js/gridvizc/BoundaryLayer.js
@@ -15,45 +15,15 @@ export class BoundaryLayer {
         opts = opts || {};
 
         /** 
-         * The URL of the label data, as CSV file.
-         * The file should contain the information for each label such as the text, the position and other information for the display of the label according to the zoom level.
-         * If necessary, this data can be reformated with the 'preprocess' parameter.
          * @private @type {string} */
         this.url = url
 
-        /** Specify if and how a label should be drawn, depending on its importance and the zoom level.
-         * @private @type {function(Label,number):string} */
-        this.style = opts.style || (() => "bold 15px Arial")
-
-        /** Specify the label color, depending on its importance and the zoom level.
-         * @private @type {function(Label,number):string} */
-        this.color = opts.color || (() => "#222")
-
-        /** Specify the label halo color, depending on its importance and the zoom level.
-         * @private @type {function(Label,number):string} */
-        this.haloColor = opts.haloColor || (() => "#FFFFFFBB")
-
-        /** Specify the label halo width, depending on its importance and the zoom level.
-        * @private @type {function(Label,number):number} */
-        this.haloWidth = opts.haloWidth || (() => 3)
-
-        /** The anchor where to draw the text, from label position. See HTML-canvas textAlign property.
-         * "left" || "right" || "center" || "start" || "end"
-         * @private @type {string} */
-        this.textAlign = opts.textAlign || "start"
-
-        /**
-        * @private @type {Array.<number>} */
-        this.offsetPix = opts.offsetPix || [5, 5]
-
         /** 
-         * A preprocess to run on each label after loading.
-         * It can be used to apply some specific treatment before, format the label data, project coordinates, etc.
          * @private @type {function(object):void} */
         this.preprocess = opts.preprocess
 
-        /** @private @type {Array.<Label> | undefined} */
-        this.labels
+        /** @private @type {Array.<object> | undefined} */
+        this.fs
 
         /** @private @type {string} */
         this.loadingStatus = "notLoaded"
@@ -61,27 +31,24 @@ export class BoundaryLayer {
 
 
     /**
-     * Draw the label layer.
-     * 
+     * Draw the layer.
      * @param {GeoCanvas} cg The canvas where to draw the layer.
      * @returns {void}
      */
     draw(cg) {
 
-        //load labels, if not done yet.
-        if (!this.labels) {
+        //load data, if not done yet.
+        if (!this.fs) {
             this.load(cg.redraw);
             return;
         }
 
+        console.log("Draw boundaries: ", this.fs)
+
         //zoom factor
-        const zf = cg.getZf()
+        //const zf = cg.getZf()
 
-        //text align
-        cg.ctx.textAlign = this.textAlign || "start";
-
-
-        //draw in pix coordinates
+        /*/draw in pix coordinates
         cg.initCanvasTransform()
 
         //draw labels, one by one
@@ -119,7 +86,7 @@ export class BoundaryLayer {
                 }
 
             }
-        }
+        }*/
     }
 
     /**
@@ -130,17 +97,20 @@ export class BoundaryLayer {
     load(callback) {
 
         if (!this.url) {
-            console.log("Failed loading labels: No URL specified. " + this.url)
+            console.log("Failed loading boundaries: No URL specified. " + this.url)
             this.loadingStatus = "failed"
             this.labels = []
             return;
         }
 
         if (this.loadingStatus === "notLoaded") {
-            this.loadingStatus = "loading"
+            this.loadingStatus = "loading";
+
+            console.log("Load boundaries here..." + this.url)
+
+            /*
             csv(this.url)
                 .then(
-                    /** @param {Array.<object>} data */
                     (data) => {
 
                         //apply preprocess, if any
@@ -160,7 +130,7 @@ export class BoundaryLayer {
                     console.log("Failed loading labels from " + this.url)
                     this.labels = []
                     this.loadingStatus = "failed"
-                });
+                });*/
         }
     }
 
@@ -172,26 +142,6 @@ export class BoundaryLayer {
     getUrl() { return this.url; }
     /** @param {string} val @returns {this} */
     setUrl(val) { this.url = val; return this; }
-
-    /** @returns {function(Label,number):string} */
-    getStyle() { return this.style; }
-    /** @param {function(Label,number):string} val @returns {this} */
-    setStyle(val) { this.style = val; return this; }
-
-    /** @returns {function(Label,number):string} */
-    getColor() { return this.color; }
-    /** @param {function(Label,number):string} val @returns {this} */
-    setColor(val) { this.color = val; return this; }
-
-    /** @returns {function(Label,number):string} */
-    getHaloColor() { return this.haloColor; }
-    /** @param {function(Label,number):string} val @returns {this} */
-    setHaloColor(val) { this.haloColor = val; return this; }
-
-    /** @returns {function(Label,number):number} */
-    getHaloWidth() { return this.haloWidth; }
-    /** @param {function(Label,number):number} val @returns {this} */
-    setHaloWidth(val) { this.haloWidth = val; return this; }
 
     /** @returns {function(object):void} */
     getPreprocess() { return this.preprocess; }
