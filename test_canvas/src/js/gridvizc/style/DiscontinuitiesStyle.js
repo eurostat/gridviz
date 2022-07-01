5b0535dd8c8dfb060690ff87310f437735ed78fa
jgaffuri <jgaf@protonmail.com>
2022-07-01 15:10:05 +0200

    a

--- test_canvas/src/js/gridvizc/style/DiscontinuitiesStyle.js
+++ test_canvas/src/js/gridvizc/style/DiscontinuitiesStyle.js
@@ -4,6 +4,9 @@ import { Style, Stat, getStatistics } from "../Style"
 import { Cell } from "../Dataset"
 import { GeoCanvas } from "../GeoCanvas";
 
+/** @typedef {{x:number,y:number,or:"v"|"h",diff:number}} Side */
+
+
 /**
  * 
  * @author Julien Gaffuri
@@ -17,13 +20,13 @@ export class DiscontinuitiesStyle extends Style {
 
         /** @private @type {string} */
         this.colorCol = opts.colorCol;
-        /** A function returning the color of the cell limit.
+        /** A function returning the color of a cell side.
         * @private @type {function(number,number,Stat|undefined):string} */
         this.color = opts.color || (() => "#EA6BAC");
 
         /** @private @type {string} */
         this.widthCol = opts.widthCol;
-        /** A function returning the width of the cell limit, in geo unit
+        /** A function returning the width of a cell side, in geo unit
          * @private @type {function(number,number,Stat|undefined,number):number} */
         this.width = opts.width;
     }
@@ -37,17 +40,19 @@ export class DiscontinuitiesStyle extends Style {
      * @param {GeoCanvas} cg 
      */
     draw(cells, r, cg) {
+
         //zoom factor
         const zf = cg.getZf()
 
+
+        //build limits
+        //{x:,y}
 
 
 
 
         //
         cg.ctx.lineCap = "butt";
-
-
 
 
     }
