5b8fb11419372a097383949bd900358cb5db0be1
jgaffuri <jgaf@protonmail.com>
2022-07-07 15:02:38 +0200

    a

--- test_canvas/src/js/gridvizc/style/TanakaStyle.js
+++ test_canvas/src/js/gridvizc/style/TanakaStyle.js
@@ -8,16 +8,15 @@ export const getTanakaStyle = function (col, opts) {
     opts = opts || {}
 
     //the colors
-    opts.colors = opts.colors //|| ["#a9bb9e", "#c9dcaa", "#f0f1af", "#fde89f", "#f9a579", "#eb444b"]
+    opts.colors = opts.colors || ["#a9bb9e", "#c9dcaa", "#f0f1af", "#fde89f", "#f9a579", "#eb444b"]
 
     //for fixed classification
     opts.breaks = opts.breaks //|| [10, 50, 200, 500, 2000]
-
     //for dynamic classification
-    opts.nbClasses = opts.nbClasses //|| 0
-    opts.valueStretch = opts.valueStretch // || ((v, r, s, zf) => str((v - s.min) / (s.max - s.min), 0.3))
+    opts.valueStretch = opts.valueStretch
 
-    opts.widthFactor = opts.widthFactor || 0.15
+    //width of the segment (share of the resolution)
+    opts.widthFactor = opts.widthFactor || 0.1
 
 
     const getClass = (v) => {
@@ -26,22 +25,21 @@ export const getTanakaStyle = function (col, opts) {
             for (let i = 0; i < opts.breaks.length; i++)
                 if (v < opts.breaks[i]) return i
             return opts.breaks.length
-        } else if (opts.nbClasses) {
+        } else if (opts.valueStretch) {
             //dynamic classification
             const t = v //within [0,1] !
-            for (let i = 0; i < opts.nbClasses - 1; i++)
-                if (t <= (i + 1) / opts.nbClasses) return i
-            return opts.nbClasses - 1
+            const nb = opts.colors.length
+            for (let i = 0; i < nb - 1; i++)
+                if (t <= (i + 1) / nb) return i
+            return nb - 1
         }
+        return NaN
     }
-
-    //const getClassColor = (i) => colFun(1 - i / (nbClasses - 1));
 
     const colStyle = new ShapeColorSizeStyle({
         colorCol: col,
         //the color corresponding to the class
         color: (v, r, s, zf) => opts.colors[getClass(v)],
-        //color: (v, r, s, zf) => getClassColor(getClass(valueStretch(v, r, s, zf))),
         shape: () => "square",
         zfStroke: 0,
     })
@@ -50,8 +48,10 @@ export const getTanakaStyle = function (col, opts) {
         valueCol: col,
         value: (v1, v2, r, s, zf) => {
             //the number of classes of difference
-            //return (v2 != undefined ? getClass(valueStretch(v2, r, s, zf)) + 1 : 0) - (v1 != undefined ? getClass(valueStretch(v1, r, s, zf)) + 1 : 0)
-            return (v2 != undefined ? getClass(v2) + 1 : 0) - (v1 != undefined ? getClass(v1) + 1 : 0)
+            const a2 = opts.valueStretch ? opts.valueStretch(v2, r, s, zf) : v2
+            const a1 = opts.valueStretch ? opts.valueStretch(v1, r, s, zf) : v1
+            return (v2 != undefined ? getClass(a2) + 1 : 0) - (v1 != undefined ? getClass(a1) + 1 : 0)
+            //return (v2 != undefined ? getClass(v2) + 1 : 0) - (v1 != undefined ? getClass(v1) + 1 : 0)
         },
         //white or black, depending on orientation
         color: (side, r, s, z) => side.value > 0 && side.or === "h" || side.value < 0 && side.or === "v" ? "#222" : "#ddd",
@@ -59,5 +59,5 @@ export const getTanakaStyle = function (col, opts) {
         width: (side, r, s, z) => opts.widthFactor * r * side.value,
     })
 
-    return [colStyle, sideStyle]
+    return [colStyle]
 }
