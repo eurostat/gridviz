72b51609fe5c1de7f801ec7c2402b33e40544f86
jgaffuri <jgaf@protonmail.com>
2022-07-08 11:15:57 +0200

    a

--- test_canvas/src/js/gridvizc/style/TanakaStyle.js
+++ test_canvas/src/js/gridvizc/style/TanakaStyle.js
@@ -22,6 +22,10 @@ export const getTanakaStyle = function (col, opts) {
     //for dynamic classification
     opts.valueStretch = opts.valueStretch
 
+    //shadow colors
+    opts.colDark = opts.colDark || "#000"
+    opts.colBright = opts.colBright || "#fff"
+
     //width of the segment (share of the resolution)
     opts.widthFactor = opts.widthFactor || 0.15
 
@@ -48,6 +52,8 @@ export const getTanakaStyle = function (col, opts) {
         //the color corresponding to the class
         color: (v, r, s, zf) => opts.colors[getClass(opts.valueStretch ? opts.valueStretch(v, r, s, zf) : v)],
         shape: () => "square",
+        sizeCol: col,
+        size: (v, r, s, zf) => r + 0.5 * zf, //that is to ensure no gap between same class cells is visible
         zfStroke: 0,
     })
 
@@ -55,14 +61,19 @@ export const getTanakaStyle = function (col, opts) {
         valueCol: col,
         value: (v1, v2, r, s, zf) => {
             //the number of classes of difference
-            const a2 = opts.valueStretch ? opts.valueStretch(v2, r, s, zf) : v2
-            const a1 = opts.valueStretch ? opts.valueStretch(v1, r, s, zf) : v1
-            return (v2 != undefined ? getClass(a2) + 1 : 0) - (v1 != undefined ? getClass(a1) + 1 : 0)
+            const v2_ = opts.valueStretch ? opts.valueStretch(v2, r, s, zf) : v2
+            const v1_ = opts.valueStretch ? opts.valueStretch(v1, r, s, zf) : v1
+            return (v2 != undefined ? getClass(v2_) + 1 : 0) - (v1 != undefined ? getClass(v1_) + 1 : 0)
         },
-        //white or black, depending on orientation
-        color: (side, r, s, z) => side.value > 0 && side.or === "h" || side.value < 0 && side.or === "v" ? "#333" : "#ccc",
+        //white or black, depending on orientation and value
+        color: (side, r, s, z) => {
+            if (side.value === 0) return
+            if (side.or === "v")
+                return side.value > 0 ? opts.colBright : opts.colDark
+            return side.value > 0 ? opts.colDark : opts.colBright
+        },
         //width depends on the value, that is the number of classes of difference
-        width: (side, r, s, z) => opts.widthFactor * r * side.value * (side.or === "v" ? 0.5 : 1),
+        width: (side, r, s, z) => opts.widthFactor * r * Math.abs(side.value) * (side.or === "v" ? 0.5 : 1),
     })
 
     return [colStyle, sideStyle]
