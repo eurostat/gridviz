7534f1457dc2d3483bad4a5cf59e43261195314b
jgaffuri <jgaf@protonmail.com>
2022-07-13 16:16:38 +0200

    a

--- test_canvas/src/js/gridvizc/style/LegoStyle.js
+++ test_canvas/src/js/gridvizc/style/LegoStyle.js
@@ -16,11 +16,11 @@ export const getLegoStyle = function (col, opts) {
 
     //the colors
     //http://www.jennyscrayoncollection.com/2021/06/all-current-lego-colors.html
-    opts.colors = opts.colors || ["blue", "red", "yellow", "green", "gray", "white", "pink"]
+    opts.colors = opts.colors || ["blue", "red", "yellow", "green", "pink"]
 
     //shadow colors
     opts.colDark = opts.colDark || "#111"
-    opts.colBright = opts.colBright || "#ddd"
+    opts.colBright = opts.colBright || "#999"
 
     //width of the segment (share of the resolution)
     opts.widthFactor = opts.widthFactor || 0.15
@@ -49,25 +49,13 @@ export const getLegoStyle = function (col, opts) {
 
     }
 
-    /** The color style */
-    /*const colStyle = new ShapeColorSizeStyle({
-        colorCol: col,
-        //the color corresponding to the class
-        color: (v, r, s, zf) => {
-            return opts.colors[Math.floor(opts.colors.length * Math.random())]
-        },
-        shape: () => "square",
-        sizeCol: col,
-        size: (v, r, s, zf) => r,
-        zfStroke: 0,
-    })*/
 
 
     /** The side style, for the shadow effect */
     const sideStyle = new SideStyle({
         valueCol: col,
         value: (v1, v2) => { return (v2 != undefined ? +v2 : 0) - (v1 != undefined ? +v1 : 0) },
-        color: (side, r, s, z) => side.value > 0 && side.or === "h" || side.value < 0 && side.or === "v" ? "black" : "white",
+        color: (side, r, s, z) => side.value > 0 && side.or === "h" || side.value < 0 && side.or === "v" ? opts.colDark : opts.colBright,
         width: (side, r, stat, z) => {
             const max = Math.max(Math.abs(stat.min), Math.abs(stat.max))
             return r * (0.01 + 0.333 * (side.or === "v" ? 0.6 : 1) * s(Math.abs(side.value) / max, 0.3))
@@ -80,15 +68,41 @@ export const getLegoStyle = function (col, opts) {
     })
 
 
-    /** The color style */
-    const topStyle = new ShapeColorSizeStyle({
-        color: (v, r, s, zf) => "none",
-        shape: () => "circle",
-        size: (v, r, s, zf) => r * 0.65,
-        zfStroke: 0.1,
-    })
+    class LegoTopStyle extends Style {
 
+        /** @param {object} opts */
+        constructor(opts = undefined) { super(opts) }
 
+        draw(cells, r, cg) {
+            cg.ctx.lineWidth = 1.5 * cg.getZf();
 
-    return [sideStyle, topStyle]
+            //dark part
+            cg.ctx.strokeStyle = opts.colDark;
+            for (let c of cells) {
+                cg.ctx.beginPath();
+                cg.ctx.arc(
+                    c.x + r * 0.5,
+                    c.y + r * 0.5,
+                    r * 0.6 * 0.5,
+                    Math.PI / 4, - Math.PI * (3 / 4), true);
+                cg.ctx.stroke();
+            }
+
+            //bright part
+            cg.ctx.strokeStyle = opts.colBright;
+            for (let c of cells) {
+                cg.ctx.beginPath();
+                cg.ctx.arc(
+                    c.x + r * 0.5,
+                    c.y + r * 0.5,
+                    r * 0.6 * 0.5,
+                    Math.PI / 4, - Math.PI * (3 / 4), false);
+                cg.ctx.stroke();
+            }
+
+        }
+
+    }
+
+    return [sideStyle, new LegoTopStyle()]
 }
