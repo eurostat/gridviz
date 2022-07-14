277185df62c5f3bc3488a81ad70eb426c1d3933f
jgaffuri <jgaf@protonmail.com>
2022-07-14 22:08:29 +0200

    a

--- test_canvas/src/js/gridvizc/style/WebGLTestStyle.js
+++ test_canvas/src/js/gridvizc/style/WebGLTestStyle.js
@@ -28,68 +28,20 @@ export class WebGLTestStyle extends Style {
         //https://webglfundamentals.org/webgl/lessons/webgl-drawing-multiple-things.html
 
 
-        cg.initCanvasTransform()
-        cg.ctx.fillStyle = "red";
-        cg.ctx.fillRect(200, 200, 100, 200);
-        cg.ctx.fillStyle = "green";
-        cg.ctx.fillRect(300, 10, 60, 100);
 
-
-        /*
-                const cv = document.createElement("canvas");
-                cv.setAttribute("width", cg.w);
-                cv.setAttribute("height", cg.h);
-                const ctx = cv.getContext("2d");
-                ctx.fillStyle = "blue";
-                ctx.fillRect(400, 60, 160, 150);
-                cg.ctx.drawImage(cv, 0, 0);
-        */
-
-
-
-
-
-        //2
-        //same but draw with webGL
-
-
-
+        //create canvas and webgl renderer
         const cv2 = document.createElement("canvas");
         cv2.setAttribute("width", cg.w);
         cv2.setAttribute("height", cg.h);
-        //document.body.appendChild(cv2);
-        //document.body.append(cv2);
-
         const gl = cv2.getContext("webgl");
         if (!gl) {
             console.error(gl, "Unable to initialize WebGL. Your browser or machine may not support it.");
-        }
-        console.log(gl)
-
-
-
-
-
-
-
-        // Initialize a shader program, so WebGL knows how to draw our data
-        function initShaderProgram(gl, ...shaders) {
-            const program = gl.createProgram();
-            for (const shader of shaders) gl.attachShader(program, shader);
-            gl.linkProgram(program);
-            if (gl.getProgramParameter(program, gl.LINK_STATUS)) return program;
-            throw new Error(gl.getProgramInfoLog(program));
+            return
         }
 
 
-        // creates a shader of the given type, uploads the source and compiles it.
-        function createShader(gl, type, ...sources) {
-            const shader = gl.createShader(type);
-            gl.shaderSource(shader, sources.join("\n"));
-            gl.compileShader(shader);
-            if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) return shader;
-            throw new Error(gl.getShaderInfoLog(shader));
-        }
+
+
 
 
         // Vertex shader program
@@ -131,14 +83,23 @@ export class WebGLTestStyle extends Style {
 
 
 
-
         {
+            // Shader uniform variable for color (read-only)
+            const color = gl.getUniformLocation(program, "color");
+
+            // Set color        R  G  B  A
+            gl.uniform4f(color, 1, 1, 0, 1);
+        }
+
+
+
+        const drawRect = function (x1, y1, x2, y2) {
             //define input vertices
             const vertices = new Float32Array([
-                0.5, 0.5,
-                -0.5, 0.5,
-                0.5, -0.5,
-                -0.5, -0.5
+                x1, y1,
+                x2, y1,
+                x1, y2,
+                x2, y2
             ]);
 
             const positionBuffer = gl.createBuffer();
@@ -154,45 +115,35 @@ export class WebGLTestStyle extends Style {
                 0 //offset
             );
             gl.enableVertexAttribArray(position);
+
+            // Draw the scene
+            //gl.clearColor(0.3, 0.3, 0.0, 1.0);
+            //gl.clearDepth(1.0); // Clear everything
+            //gl.enable(gl.DEPTH_TEST); // Enable depth testing
+            //gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+            // Clear the canvas before we start drawing on it.
+            //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.drawArrays(
+                gl.TRIANGLE_STRIP, // mode,see https://miro.medium.com/max/700/0*HQHB5lCGqlOUiysy.jpg
+                0, // vertex list start
+                4 // vertex count
+            );
+
+
         }
 
-
-        {
-            // Shader uniform variable for color (read-only)
-            const color = gl.getUniformLocation(program, "color");
-
-            // Set color        R  G  B  A
-            gl.uniform4f(color, 0, 1, 0, 1);
-        }
-
-        // Set the shader uniforms
-        /*
-        gl.uniformMatrix4fv(
-            programInfo.uniformLocations.projectionMatrix,
-            false,
-            projectionMatrix);
-        gl.uniformMatrix4fv(
-            programInfo.uniformLocations.modelViewMatrix,
-            false,
-            modelViewMatrix);
-      */
-
-        // Draw the scene
-        //gl.clearColor(0.3, 0.3, 0.0, 1.0);
-        gl.clearDepth(1.0); // Clear everything
-        gl.enable(gl.DEPTH_TEST); // Enable depth testing
-        gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-        // Clear the canvas before we start drawing on it.
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.drawArrays(
-            gl.TRIANGLE_STRIP, // mode,see https://miro.medium.com/max/700/0*HQHB5lCGqlOUiysy.jpg
-            0, // vertex list start
-            4 // vertex count
-        );
+        drawRect(0, 0.1, 0.3, 0.3)
+        drawRect(-0.2, -0.9, 0.9, 0)
 
 
 
 
+
+
+
+
+        //draw in canvas geo
+        cg.initCanvasTransform()
         cg.ctx.drawImage(cv2, 0, 0);
 
 
@@ -208,4 +159,24 @@ export class WebGLTestStyle extends Style {
         //use webGL mode in relevant styles
     }
 
+}
+
+
+// Initialize a shader program, so WebGL knows how to draw our data
+function initShaderProgram(gl, ...shaders) {
+    const program = gl.createProgram();
+    for (const shader of shaders) gl.attachShader(program, shader);
+    gl.linkProgram(program);
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) return program;
+    throw new Error(gl.getProgramInfoLog(program));
+}
+
+
+// creates a shader of the given type, uploads the source and compiles it.
+function createShader(gl, type, ...sources) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, sources.join("\n"));
+    gl.compileShader(shader);
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) return shader;
+    throw new Error(gl.getShaderInfoLog(shader));
 }
