1b6c507219c4682b0d5f871860df9c227d09eba5
jgaffuri <jgaf@protonmail.com>
2022-07-15 23:50:02 +0200

    a

---
+++ test_canvas/src/js/gridvizc/utils/WebGLSquareColoring.js
@@ -0,0 +1,103 @@
+//@ts-check
+
+import { initShaderProgram, createShader } from "./webGLUtils";
+
+
+/**
+ * Aything to easily draw colored points (square) with webGL.
+ */
+export class WebGLSquareColoring {
+
+    /**
+     * 
+     * @param {*} gl 
+     */
+    constructor(gl) {
+
+        this.gl = gl
+
+        this.program = initShaderProgram(
+            gl,
+            createShader(gl, gl.VERTEX_SHADER, `
+            attribute vec2 pos;
+            //attribute float size;
+            uniform mat3 mat;
+            attribute vec3 color;
+            varying vec3 vColor;
+            void main() {
+              gl_Position = vec4(mat * vec3(pos, 1.0), 1.0);
+              gl_PointSize = 1000.0
+              vColor = color;
+            }
+          `),
+            createShader(gl, gl.FRAGMENT_SHADER, `
+            precision mediump float;
+            varying vec3 vColor;
+            void main(void) {
+               gl_FragColor = vec4(vColor, 1.0);
+            }`)
+        );
+        gl.useProgram(this.program);
+
+        //buffer data
+        this.verticesBuffer = [];
+        this.sizeBuffer = [];
+        this.colorsBuffer = [];
+    }
+
+    /** Add data to vertices/size/color buffers for color squares drawing */
+    addPointData(x, y, size, cR = 0, cG = 0, cB = 1, cA = 0) { //TODO cA
+        //add vertices
+        const v = this.verticesBuffer
+        v.push(x); v.push(y)
+
+        //add size
+        this.sizeBuffer.push(size)
+
+        //colors, 3 parts (RGB), one per vertice
+        const c = this.colorsBuffer
+        c.push(cR); c.push(cG); c.push(cB)
+        c.push(cR); c.push(cG); c.push(cB)
+        c.push(cR); c.push(cG); c.push(cB)
+        c.push(cR); c.push(cG); c.push(cB)
+    }
+
+
+    /**  */
+    draw(transfoMat) {
+        const gl = this.gl
+
+        //vertice data
+        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.verticesBuffer), gl.STATIC_DRAW);
+        const position = gl.getAttribLocation(this.program, "pos");
+        gl.vertexAttribPointer(
+            position,
+            2, //numComponents
+            gl.FLOAT, //type
+            false, //normalise
+            0, //stride
+            0 //offset
+        );
+        gl.enableVertexAttribArray(position);
+
+        //color data
+        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colorsBuffer), gl.STATIC_DRAW);
+        var color = gl.getAttribLocation(this.program, "color");
+        gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(color);
+
+        //transformation
+        gl.uniformMatrix3fv(gl.getUniformLocation(this.program, "mat"), false, new Float32Array(transfoMat));
+
+        // Enable the depth test
+        //gl.enable(gl.DEPTH_TEST);
+        // Clear the color buffer bit
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        // Set the view port
+        //gl.viewport(0, 0, cg.w, cg.h);
+
+        gl.drawArrays(gl.POINTS, 0, this.verticesBuffer.length/2)
+    }
+}
