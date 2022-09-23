b8e22cc2f9416ee710dd9398f1c261ef90f3dbe3
jgaffuri <jgaf@protonmail.com>
2022-09-23 21:52:47 +0200

    a

--- src/js/style/LegoStyle.js
+++ src/js/style/LegoStyle.js
@@ -2,22 +2,20 @@
 
 import { SquareColorWGLStyle2 } from "./SquareColorWGLStyle2"
 import { SideStyle } from "./SideStyle"
+import { TanakaStyle } from "./TanakaStyle"
 import { Style } from "../Style"
 
 /**
- * 
  * @author Julien Gaffuri
  */
 export class LegoStyle {
 
     /**
-     * 
      * @param {string} col 
      * @param {object} opts 
      * @returns {Array.<Style>}
      */
     static get(col, opts) {
-
         opts = opts || {}
 
         //the colors
@@ -34,103 +32,9 @@ export class LegoStyle {
             "#720012", //dark red
         ]
 
-        //for fixed classification
-        opts.breaks = opts.breaks
-        //for dynamic classification
-        opts.tFun = opts.tFun
-
-        //shadow colors
         opts.colDark = opts.colDark || "#333"
         opts.colBright = opts.colBright || "#aaa"
-
-        //width of the segment (share of the resolution)
         opts.widthFactor = opts.widthFactor || 0.05
-
-        /**
-         * @param {number} v A cell value (within [0,1] for a dynamic classification).
-         * @returns the class number of the value
-         */
-        const getClass = (v) => {
-
-            //fixed classification
-            if (opts.breaks) {
-                for (let i = 0; i < opts.breaks.length; i++)
-                    if (v < opts.breaks[i]) return i
-                return opts.breaks.length
-
-                //dynamic classification
-            } else if (opts.tFun) {
-                const t = v //within [0,1] !
-                const nb = opts.colors.length
-                for (let i = 0; i < nb - 1; i++)
-                    if (t <= (i + 1) / nb) return i
-                return nb - 1
-            }
-            return NaN
-
-        }
-
-
-
-
-        /** The color style */
-        const colStyle = new SquareColorWGLStyle2({
-            colorCol: col,
-            //the color corresponding to the class
-            color: (v, r, s, zf) => {
-                if (v == 0 && opts.tFun && isNaN(opts.tFun(v, r, s, zf)))
-                    return undefined
-                return opts.colors[getClass(opts.tFun ? opts.tFun(v, r, s, zf) : v)]
-            },
-        })
-
-        /*
-        if no webgl:
-        const colStyle = new ShapeColorSizeStyle({
-            colorCol: col,
-            //the color corresponding to the class
-            color: (v, r, s, zf) => {
-                if (v == 0 && opts.tFun && isNaN(opts.tFun(v, r, s, zf)))
-                    return undefined
-                return opts.colors[getClass(opts.tFun ? opts.tFun(v, r, s, zf) : v)]
-            },
-            shape: () => "square",
-        })
-    */
-
-        /** The side style, for the shadow effect */
-        const sideStyle = new SideStyle({
-            valueCol: col,
-            value: (v1, v2, r, s, zf) => {
-                //the number of classes of difference
-
-                //Stretch values, if necessary
-                const v1_ = opts.tFun ? opts.tFun(v1, r, s, zf) : v1
-                const v2_ = opts.tFun ? opts.tFun(v2, r, s, zf) : v2
-                //if no v1, no v2
-                if (((!v1_ || isNaN(v1_)) && (!v2_ || isNaN(v2_)))) return 0;
-                //if no v1
-                else if (!v1_ || isNaN(v1_)) return getClass(v2_) //+1
-                //if no v2
-                else if (!v2_ || isNaN(v2_)) return -getClass(v1_) //-1
-                //else, difference between two class numbers
-                return getClass(v2_) - getClass(v1_);
-            },
-            //white or black, depending on orientation and value
-            color: (side, r, s, z) => {
-                if (side.value === 0) return opts.colDark
-                if (side.or === "v")
-                    return side.value > 0 ? opts.colBright : opts.colDark
-                return side.value > 0 ? opts.colDark : opts.colBright
-            },
-            //width depends on the value, that is the number of classes of difference
-            width: (side, r, s, z) => {
-                //return 1*z
-                const v = side.value;
-                if (v == 0) return 0.75 * z
-                return opts.widthFactor * r * Math.abs(side.value) * (side.or === "v" ? 0.5 : 1)
-            },
-        })
 
         class LegoTopStyle extends Style {
 
@@ -168,6 +72,9 @@ export class LegoStyle {
 
         }
 
-        return [colStyle, sideStyle, new LegoTopStyle()]
+        const out = TanakaStyle.get(col, opts)
+        out.push(new LegoTopStyle())
+
+        return out
     }
 }
