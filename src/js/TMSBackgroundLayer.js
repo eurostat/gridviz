d305ec1d7f10c7673d6645e9d672c5d27f16830a
jgaffuri <jgaf@protonmail.com>
2022-09-26 13:57:20 +0200

    a

--- src/js/TMSBackgroundLayer.js
+++ src/js/TMSBackgroundLayer.js
@@ -21,20 +21,21 @@ export class TMSBackgroundLayer {
 
         /** An attribute to specify if a layer should be drawn or not
          * @type {boolean} */
-         this.visible = opts.visible == false ? false : true;
+        this.visible = opts.visible == false ? false : true;
 
-         /** The minimum zoom factor: Below this level, the layer is not shown.
-          * @type {number} */
-         this.minZoom = opts.minZoom || 0;
- 
-         /** The maximum zoom factor: Above this level, the layer is not shown.
-          * @type {number} */
-         this.maxZoom = opts.maxZoom || Infinity;
- 
-         //ensure acceptable values for the zoom limits.
-         if (this.minZoom >= this.maxZoom)
-             throw new Error("Unexpected zoom limits for layer. Zoom min should be smaller than zoom max.")
- 
+        /** The minimum zoom factor: Below this level, the layer is not shown.
+         * @type {number} */
+        this.minZoom = opts.minZoom || 0;
+
+        /** The maximum zoom factor: Above this level, the layer is not shown.
+         * @type {number} */
+        this.maxZoom = opts.maxZoom || Infinity;
+
+        //ensure acceptable values for the zoom limits.
+        if (this.minZoom >= this.maxZoom)
+            throw new Error("Unexpected zoom limits for layer. Zoom min should be smaller than zoom max.")
+
+        this.img = undefined
     }
 
 
@@ -44,7 +45,34 @@ export class TMSBackgroundLayer {
      */
     draw(cg) {
 
+        const url = this.url + "3/8/7";
+        console.log(url)
+
         //TODO
+
+        if(!this.img) {
+            this.img = new Image()
+            this.img.src = url
+            this.img.onload = function () {
+                cg.redraw()
+            }
+            return;
+        }
+
+        cg.ctx.drawImage(this.img, 100, 100)
+
+
+        /*
+        async function drawImage(url, ctx) {
+          let img = new Image();
+          await new Promise(r => img.onload=r, img.src=url);
+          ctx.drawImage(img, 0, 0);
+        }
+        
+        let ctx = document.querySelector("#myCanvas").getContext("2d");
+        await drawImage("https://example.com/image.png", ctx);
+        */
+
 
     }
 
