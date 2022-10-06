a81f2e5ee24bf5a2139da34a1d54ad305c476bae
jgaffuri <jgaf@protonmail.com>
2022-10-06 14:52:37 +0200

    a

--- src/js/utils/WebGLSquareColoringCatAdvanced.js
+++ src/js/utils/WebGLSquareColoringCatAdvanced.js
@@ -83,18 +83,18 @@ export class WebGLSquareColoringCatAdvanced {
         const program = initShaderProgram(gl, vShader, fShader);
         gl.useProgram(program);
 
+
         //set uniforms
 
         //sizePix
         gl.uniform1f(gl.getUniformLocation(program, "sizePix"), 1.0 * sizePix);
 
         //colors
-        //gl.uniform4fv(gl.getUniformLocation(this.program, "c0"), [0.3, 0, 0.8, 0.5]);
-        //TODO one uniform per color, indexed by i. Possible to use an array of colors?
         for (let i = 0; i < this.colors.length; i++) {
             const c = color(this.colors[i])
             gl.uniform4fv(gl.getUniformLocation(program, "c" + i), [+c.r / 255.0, +c.g / 255.0, +c.b / 255.0, +c.opacity]);
         }
+
 
         //vertice data
         gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
