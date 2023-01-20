6d388b8c5f10ae819139d0334911f3af7afa87c9
jgaffuri <jgaf@proton.me>
2023-01-20 09:56:53 +0100

    style template

---
+++ src/style/MyStyle.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+import { Style } from "../Style"
+
+export class MyStyle extends Style {
+
+    constructor(opts) {
+        super(opts)
+        opts = opts || {};
+
+        //load style parameters + set default values
+
+    }
+
+    draw(cells, resolution, canvas) {
+
+        //draw there !
+
+    }
+
+}
