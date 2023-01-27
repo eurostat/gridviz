c8162cef3b42f19ef277eb78bc913004435ea73b
jgaffuri <jgaf@proton.me>
2023-01-27 11:53:52 +0100

    text style fisture

--- src/style/TextStyle.js
+++ src/style/TextStyle.js
@@ -37,7 +37,7 @@ export class TextStyle extends Style {
 
         /** A function returning the font size of a cell in geo unit.
         * @type {function(number,number,import("../Style").Stat|undefined,number):number} */
-        this.fontSize = opts.fontSize || ((v, r, s, z) => r - 2);
+        this.fontSize = opts.fontSize || ((v, r, s, z) => r * 0.8);
 
 
 
@@ -94,15 +94,14 @@ export class TextStyle extends Style {
             cg.ctx.fillStyle = col;
 
             //font size
-            /** @type {function(number,number,import("../Style").Stat|undefined,number):number} */
-            let s_ = this.fontSize || (() => r / zf - 2);
             //size - in pixel unit
-            const fontSizePix = s_(cell[this.fontSizeCol], r, statFontSize, zf) / zf;
+            const fontSizePix = this.fontSize(cell[this.fontSizeCol], r, statFontSize, zf) / zf;
 
             //set font
             const fontFamily = this.fontFamily || "Arial";
             const fontWeight = this.fontWeight || "bold";
             cg.ctx.font = fontWeight + " " + fontSizePix + "px " + fontFamily;
+            console.log(cg.ctx.font)
 
             //get offset
             const offset = this.offset(cell, r, zf)
@@ -110,7 +109,7 @@ export class TextStyle extends Style {
             //text position
             cg.ctx.textAlign = "center";
             const tx = cg.geoToPixX(cell.x + r * 0.5 + offset.dx);
-            const ty = cg.geoToPixY(cell.y + r * 0.5 + offset.dy) + fontSizePix * 0.5;
+            const ty = cg.geoToPixY(cell.y + r * 0.5 + offset.dy) + fontSizePix * 0.3;
 
             //draw the text
             cg.ctx.fillText(text, tx, ty);
