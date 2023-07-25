e102a13a927943fff9a06c6c977f5559a7d5a6c6
jgaffuri <jgaf@proton.me>
2023-07-25 14:36:37 +0200

    a

--- src/style/SparklineStyle.js
+++ src/style/SparklineStyle.js
@@ -18,6 +18,15 @@ export class SparklineStyle extends Style {
          * @type {Array.<string>} */
         this.ts = opts.ts
 
+
+        /**
+         * @type {string} */
+        this.widthCol = opts.widthCol
+
+        /** A function returning the width of the line, in geo unit
+         * @type {function(number,number,import("../Style").Stat|undefined,number):number} */
+        this.width = opts.width ||  ((v,r,s,z) => 1*z)
+
     }
 
     /**
@@ -34,6 +43,12 @@ export class SparklineStyle extends Style {
 
         //zoom factor
         const zf = cg.getZf()
+
+        let statWidth
+        if (this.widthCol) {
+            //and compute size variable statistics
+            statWidth = Style.getStatistics(cells, (c) => c[this.widthCol], true)
+        }
 
         //compute cell amplitude
         const getAmplitude = c => {
@@ -55,24 +70,52 @@ export class SparklineStyle extends Style {
             if (amp == undefined) continue
             if (ampMax == undefined || amp > ampMax) ampMax = amp
         }
+        if (!ampMax) return
 
 
         //draw with HTML canvas
         //in geo coordinates
         cg.setCanvasTransform()
 
-        cg.ctx.strokeStyle = "red"
-        cg.ctx.lineWidth = 5 * zf
+        cg.ctx.strokeStyle = "black"
+        //
+        cg.ctx.lineCap = 'butt'
 
-        const r2 = r * 0.5
-        for (let cell of cells) {
-            //center position
-            const cx = cell.x + r2
-            const cy = cell.y + r2
+        const offX = 0 //TODO
+        const offY = 0 //TODO
+        const width = r
+        const height = r
+        const nb = this.ts.length
+        const stepX = width / (nb - 1)
 
+        for (let c of cells) {
+
+            //width
+            /** @type {number|undefined} */
+            const wG = this.width ? this.width(c[this.widthCol], r, statWidth, zf) : undefined
+            if (!wG || wG < 0) continue
+
+            cg.ctx.lineWidth = wG
+
+            /*/get min value
+            let minY
+            {
+                for (let t of this.ts) {
+                    const val = c[t]
+                    if (minY == undefined || val < minY) minY = val
+                }
+            }*/
+
+            //draw line
             cg.ctx.beginPath()
-            cg.ctx.moveTo(cx, cy)
-            cg.ctx.lineTo(cx + r2, cy + r2)
+            const val0 = c[this.ts[0]]
+            for (let i = 0; i < nb; i++) {
+                const val = c[this.ts[i]]
+                if (i == 0)
+                    cg.ctx.moveTo(c.x, c.y)
+                else
+                    cg.ctx.lineTo(c.x + i * stepX, c.y + (val - val0) * height / ampMax)
+            }
             cg.ctx.stroke()
 
         }
