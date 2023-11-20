e7cfa219041d4d1877b87b2772311ade036cee77
jgaffuri <jgaf@proton.me>
2023-11-20 10:01:34 +0100

    new stretching functions

--- src/utils/stretching.js
+++ src/utils/stretching.js
@@ -1,6 +1,25 @@
 //@ts-check
 'use strict'
 
+
+
+//stretching functions [0,1]=>[0,1]
+
+export const powerScale = (exponent = 3) => t => Math.pow(t, exponent)
+export const powerReverseScale = (exponent = 3) => t => 1 - Math.pow(1 - t, 1 / exponent)
+export const exponentialScale = (base = 3) => {
+    if (base == 0) return t => t
+    const a = (Math.exp(base) - 1)
+    return t => (Math.exp(t * base) - 1) / a
+}
+export const logarithmicScale = (base = 3) => {
+    if (base == 0) return t => t
+    return t => 1 - (1 / base) * Math.log(Math.exp(base) * (1 - t) + t)
+}
+
+
+
+
 /**
  * Some function [0,1]->[0,1] to stretch range of values.
  * @see https://github.com/eurostat/gridviz/blob/master/docs/reference.md#stretching
@@ -12,41 +31,41 @@
  * Polynomial
  *
  * @param {number} t The value to stretch, within [0,1]
- * @param {number} alpha 1: no stretching. <1: show low values. >1: show high values.
+ * @param {number} exponent 1: no stretching. <1: show low values. >1: show high values.
  * @returns {number} The stretched value, within [0,1]
  */
-export const sPow = (t, alpha = 3) => Math.pow(t, alpha)
+export const sPow = (t, exponent = 3) => Math.pow(t, exponent)
 
 /**
  * Function [0,1]->[0,1] to stretch range of values.
  * Polynomial (reverse)
  *
  * @param {number} t The value to stretch, within [0,1]
- * @param {number} alpha 1: no stretching. <1: show low values. >1: show high values.
+ * @param {number} exponent 1: no stretching. <1: show low values. >1: show high values.
  * @returns {number} The stretched value, within [0,1]
  */
-export const sPowRev = (t, alpha = 3) => 1 - Math.pow(1 - t, 1 / alpha)
+export const sPowRev = (t, exponent = 3) => 1 - Math.pow(1 - t, 1 / exponent)
 
 /**
  * Function [0,1]->[0,1] to stretch range of values.
  * Exponential
  *
  * @param {number} t The value to stretch, within [0,1]
- * @param {number} alpha 0: no stretching. -Inf: show low values. Inf: show high values.
+ * @param {number} base 0: no stretching. -Inf: show low values. Inf: show high values.
  * @returns {number} The stretched value, within [0,1]
  */
-export const sExp = (t, alpha = 3) => (alpha == 0 ? t : (Math.exp(t * alpha) - 1) / (Math.exp(alpha) - 1))
+export const sExp = (t, base = 3) => (base == 0 ? t : (Math.exp(t * base) - 1) / (Math.exp(base) - 1))
 
 /**
  * Function [0,1]->[0,1] to stretch range of values.
  * Exponential (reverse)
  *
  * @param {number} t The value to stretch, within [0,1]
- * @param {number} alpha 0: no stretching. -Inf: show low values. Inf: show high values.
+ * @param {number} base 0: no stretching. -Inf: show low values. Inf: show high values.
  * @returns {number} The stretched value, within [0,1]
  */
-export const sExpRev = (t, alpha = 3) =>
-    alpha == 0 ? t : 1 - (1 / alpha) * Math.log(Math.exp(alpha) * (1 - t) + t)
+export const sExpRev = (t, base = 3) =>
+    base == 0 ? t : 1 - (1 / base) * Math.log(Math.exp(base) * (1 - t) + t)
 
 /**
  * Function [0,1]->[0,1] to stretch range of values.
@@ -82,36 +101,36 @@ export const sCircleHigh = (t, alpha = 0.8) => 1 - sCircleLow(1 - t, alpha)
 /**
  * Inverse function of sExp
  * @param {number} y
- * @param {number} alpha
+ * @param {number} base
  * @returns {number}
  */
-export const sExpInverse = (y, alpha = 3) =>
-    alpha == 0 ? y : (1 / alpha) * Math.log(1 - y + y * Math.exp(alpha))
+export const sExpInverse = (y, base = 3) =>
+    base == 0 ? y : (1 / base) * Math.log(1 - y + y * Math.exp(base))
 
 /**
  * Inverse function of sExpRev
  * @param {number} y
- * @param {number} alpha
+ * @param {number} base
  * @returns {number}
  */
-export const sExpRevInverse = (y, alpha = 3) => (Math.exp(-alpha * y) - 1) / (Math.exp(-alpha) - 1)
+export const sExpRevInverse = (y, base = 3) => (Math.exp(-base * y) - 1) / (Math.exp(-base) - 1)
 
 /**
  * Inverse function of sPow
  * @param {number} y
- * @param {number} alpha
+ * @param {number} exponent
  * @returns {number}
  */
 
-export const sPowInverse = (y, alpha = 3) => Math.pow(y, 1 / alpha)
+export const sPowInverse = (y, exponent = 3) => Math.pow(y, 1 / exponent)
 
 /**
  * Inverse function of sPowRev
  * @param {number} y
- * @param {number} alpha
+ * @param {number} exponent
  * @returns {number}
  */
-export const sPowRevInverse = (y, alpha = 3) => 1 - Math.pow(1 - y, alpha)
+export const sPowRevInverse = (y, exponent = 3) => 1 - Math.pow(1 - y, exponent)
 
 //test code
 /*
