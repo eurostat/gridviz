fe048520afd7af8c388805ef5f8b22fce50c466d
jgaffuri <jgaf@proton.me>
2023-11-24 14:21:46 +0100

    structure change

--- src/utils/stretching.js
+++ src/utils/stretching.js
@@ -9,18 +9,35 @@
  */
 
 
+/**
+ * @param {number} exponent 
+ * @returns {function(number):number}
+ */
 export const powerScale = (exponent = 3) => t => Math.pow(t, exponent)
 
+/**
+ * @param {number} exponent 
+ * @returns {function(number):number}
+ */
 export const powerInverseScale = (exponent = 3) => t => 1 - Math.pow(1 - t, 1 / exponent)
 
 
 
+
+/**
+ * @param {number} base 
+ * @returns {function(number):number}
+ */
 export const exponentialScale = (base = 3) => {
     if (base == 0) return t => t
     const a = (Math.exp(base) - 1)
     return t => (Math.exp(t * base) - 1) / a
 }
 
+/**
+ * @param {number} base 
+ * @returns {function(number):number}
+ */
 export const logarithmicScale = (base = 3) => {
     if (base == 0) return t => t
     return t => 1 - (1 / base) * Math.log(Math.exp(base) * (1 - t) + t)
@@ -28,6 +45,12 @@ export const logarithmicScale = (base = 3) => {
 
 
 
+
+
+/**
+ * @param {number} alpha 
+ * @returns {function(number):number}
+ */
 export const circularScale = (alpha = 0.8) => {
     if (alpha == 0) return t => t
     if (alpha == 1) return t => Math.sqrt(t * (2 - t))
@@ -36,6 +59,11 @@ export const circularScale = (alpha = 0.8) => {
         return t => Math.sqrt(1 / (a * a) + t * (2 / a + 2 - t)) - 1 / a
     }
 }
+
+/**
+ * @param {number} alpha 
+ * @returns {function(number):number}
+ */
 export const circularInverseScale = (alpha = 0.8) => {
     const f = circularScale(alpha)
     return t => 1 - f(1 - t)
