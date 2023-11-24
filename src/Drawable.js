840f7bf88cf8fd08740cd902338671442d02f45e
jgaffuri <jgaf@proton.me>
2023-11-24 15:52:42 +0100

    structure change

--- src/Drawable.js
+++ src/Drawable.js
@@ -2,6 +2,8 @@
 'use strict'
 
 /**
+ * This is an abstract class used to group elements shared between Layer and Style classes.
+ * 
  * @abstract
  * @author Joseph Davies, Julien Gaffuri
  */
@@ -12,29 +14,30 @@ export class Drawable {
     constructor(opts) {
         opts = opts || {}
 
-        /** An attribute to specify if a layer should be drawn or not
+        /** An attribute to specify if the object should be drawn or not
         * @type {boolean} */
         this.visible = opts.visible != false
 
-        /** The minimum : Below this level, the layer is not shown.
+        /** The minimum : Below this level, the object is not drawn.
          * @type {number} */
         this.minZoom = opts.minZoom || 0
 
-        /** The maximum : Above this level, the layer is not shown.
+        /** The maximum : Above this level, the object is not drawn.
          * @type {number} */
         this.maxZoom = opts.maxZoom || Infinity
 
         //ensure acceptable values for the zoom limits.
         if (this.minZoom >= this.maxZoom)
-            throw new Error('Unexpected zoom limits for layer. Zoom min should be smaller than zoom max.')
+            throw new Error('Unexpected zoom limits for element. Zoom min should be smaller than zoom max.')
 
         /** A function returning the alpha (transparency/opacity), between 0.0 (fully transparent) and 1.0 (fully opaque).
-         *  The function parameter is the .
+         *  The function parameter is the zoom level.
          * (see CanvasRenderingContext2D: globalAlpha property)
          * @type {(function(number):number)|undefined} */
         this.alpha = opts.alpha
 
-        /** A function returning the blend operation. The function parameter is the .
+        /** A function returning the blend operation.
+         * The function parameter is the zoom level.
          * (see CanvasRenderingContext2D: globalCompositeOperation property)
          * @type {function(number):GlobalCompositeOperation} */
         this.blendOperation = opts.blendOperation || (z => "source-over")
