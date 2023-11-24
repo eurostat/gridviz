897d9395131cbc7a3a5904fc0b81518e0a93a5be
jgaffuri <jgaf@proton.me>
2023-11-24 15:47:56 +0100

    structure change

--- src/Style.js
+++ src/Style.js
@@ -1,6 +1,8 @@
 //@ts-check
 'use strict'
 
+import { Drawable } from './Drawable.js'
+
 /**
  * Statistics of a set of values
  * @typedef {{min:number,max:number}} Stat */
@@ -16,45 +18,16 @@
  *
  * @author Joseph Davies, Julien Gaffuri
  */
-export class Style {
+export class Style extends Drawable {
     /**
      * @abstract
      * @param {{filter?:function(import('./Dataset').Cell):boolean,offset?:function(import('./Dataset').Cell,number,number):{dx:number,dy:number},visible?:boolean,alpha?:function(number):number,blendOperation?:function(number):GlobalCompositeOperation,minZoom?:number,maxZoom?:number,drawFun?:function,viewScale?:ViewScale}} opts
      */
     constructor(opts) {
+        super(opts)
         opts = opts || {}
 
-        /** An attribute to specify if a style should be drawn or not
-         * @type {boolean} */
-        this.visible = opts.visible === false ? false : true
-
-        /** The minimum : Below this level, the layer is not shown.
-         * @type {number}
-         * */
-        this.minZoom = opts.minZoom || 0
-
-        /** The maximum : Above this level, the layer is not shown.
-         * @type {number}
-         * */
-        this.maxZoom = opts.maxZoom || Infinity
-
-        //ensure acceptable values for the zoom limits.
-        if (this.minZoom >= this.maxZoom)
-            throw new Error('Unexpected zoom limits for layer. Zoom min should be smaller than zoom max.')
-
-        /** A function returning the alpha (transparency/opacity), between 0.0 (fully transparent) and 1.0 (fully opaque).
-         *  The function parameter is the .
-         * (see CanvasRenderingContext2D: globalAlpha property)
-         * @type {(function(number):number)|undefined} */
-        this.alpha = opts.alpha
-
-        /** A function returning the blend operation. The function parameter is the .
-         * (see CanvasRenderingContext2D: globalCompositeOperation property)
-         * @type {function(number):GlobalCompositeOperation} */
-        this.blendOperation = opts.blendOperation || (z => "source-over")
-
-
-            /**
+        /**
         * @type {ViewScale|undefined} */
         this.viewScale = opts.viewScale
 
