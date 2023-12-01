61002353ae12f1e72c9889dd942e6b27a967d9ea
jgaffuri <jgaf@proton.me>
2023-12-01 17:39:11 +0100

    structure change

--- src/style/SegmentStyle.js
+++ src/style/SegmentStyle.js
@@ -14,33 +14,21 @@ export class SegmentStyle extends Style {
         super(opts)
         opts = opts || {}
 
-        /** A function returning the orientation (in degrees) of the segment representing a cell.
-         * @type {function(import("../core/Dataset").Cell):number} */
-        this.orientation = opts.orientation || (() => 0)
-
-        /**
-         * @type {string} */
-        this.colorCol = opts.colorCol
-
         /** A function returning the color of the cell segment.
-         * @type {function(number,number,import("../core/Style").Stat|undefined):string} */
-        this.color = opts.color || (() => '#EA6BAC')
-
-        /**
-         * @type {string} */
-        this.lengthCol = opts.lengthCol
-
-        /** A function returning the length of the segment representing a cell, in geo unit
-         * @type {function(number,number,import("../core/Style").Stat|undefined,number):number} */
-        this.length = opts.length
-
-        /**
-         * @type {string} */
-        this.widthCol = opts.widthCol
+         * @type {function(import('../core/Dataset.js').Cell, number, number, object):string} */
+        this.color = opts.color || (() => "#EA6BAC") //(c,r,z,vs) => {}
 
         /** A function returning the width of the segment representing a cell, in geo unit
-         * @type {function(number,number,import("../core/Style").Stat|undefined,number):number} */
-        this.width = opts.width
+         * @type {function(import('../core/Dataset.js').Cell, number, number, object):number} */
+        this.width = opts.width || ((cell, resolution) => resolution * 0.1)  //(c,r,z,vs) => {}
+
+        /** A function returning the length of the segment representing a cell, in geo unit
+         * @type {function(import('../core/Dataset.js').Cell, number, number, object):number} */
+        this.length = opts.length || ((cell, resolution) => resolution * 0.9)  //(c,r,z,vs) => {}
+
+        /** A function returning the orientation (in degrees) of the segment representing a cell.
+         * @type {function(import('../core/Dataset.js').Cell, number, number, object):number} */
+        this.orientation = opts.orientation || (() => 180 * Math.random()) //(c,r,z,vs) => {}
     }
 
     /**
@@ -57,25 +45,8 @@ export class SegmentStyle extends Style {
         //
         const z = geoCanvas.view.z
 
-        let statColor
-        if (this.colorCol) {
-            //compute color variable statistics
-            statColor = Style.getStatistics(cells, (c) => c[this.colorCol], true)
-        }
-
-        let statLength
-        if (this.lengthCol) {
-            //if length is used, sort cells by length so that the longests are drawn first
-            cells.sort((c1, c2) => c2[this.lengthCol] - c1[this.lengthCol])
-            //and compute size variable statistics
-            statLength = Style.getStatistics(cells, (c) => c[this.lengthCol], true)
-        }
-
-        let statWidth
-        if (this.widthCol) {
-            //and compute size variable statistics
-            statWidth = Style.getStatistics(cells, (c) => c[this.widthCol], true)
-        }
+        //get view scale
+        const viewScale = this.viewScale ? this.viewScale(cells, resolution, z) : undefined
 
         //
         geoCanvas.ctx.lineCap = 'butt'
@@ -83,37 +54,37 @@ export class SegmentStyle extends Style {
         //conversion factor degree -> radian
         const f = Math.PI / 180
 
-        for (let c of cells) {
+        for (let cell of cells) {
             //color
             /** @type {string|undefined} */
-            const col = this.color ? this.color(c[this.colorCol], resolution, statColor) : undefined
+            const col = this.color ? this.color(cell, resolution, z, viewScale) : undefined
             if (!col) continue
 
             //width
             /** @type {number|undefined} */
-            const wG = this.width ? this.width(c[this.widthCol], resolution, statWidth, z) : undefined
+            const wG = this.width ? this.width(cell, resolution, z, viewScale) : undefined
             if (!wG || wG < 0) continue
 
             //length
             /** @type {number|undefined} */
-            const lG = this.length ? this.length(c[this.lengthCol], resolution, statLength, z) : undefined
+            const lG = this.length ? this.length(cell, resolution, z, viewScale) : undefined
             if (!lG || lG < 0) continue
 
             //orientation (in radian)
             /** @type {number} */
-            const or = this.orientation(c) * f
+            const or = this.orientation(cell, resolution, z, viewScale) * f
             if (or === undefined || isNaN(or)) continue
 
             //get offset
-            const offset = this.offset(c, resolution, z)
+            const offset = this.offset(cell, resolution, z)
 
             //set color and width
             geoCanvas.ctx.strokeStyle = col
             geoCanvas.ctx.lineWidth = wG
 
             //compute segment centre postition
-            const cx = c.x + resolution / 2 + offset.dx
-            const cy = c.y + resolution / 2 + offset.dy
+            const cx = cell.x + resolution / 2 + offset.dx
+            const cy = cell.y + resolution / 2 + offset.dy
 
             //compute segment direction
             const dx = 0.5 * Math.cos(or) * lG
@@ -128,12 +99,10 @@ export class SegmentStyle extends Style {
 
         //update legend, if any
         this.updateLegends({
-            widthFun: this.width,
-            r: resolution,
+            style: this,
+            resolution: resolution,
             z: z,
-            sColor: statColor,
-            //sLength: statLength,
-            sWidth: statWidth,
+            viewScale: viewScale
         })
     }
 }
