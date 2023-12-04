1261587cbe8c291182acdc7362b808cd3f7b7551
jgaffuri <jgaf@proton.me>
2023-12-04 20:02:20 +0100

    structure change

--- src/legend/SegmentOrientationLegend.js
+++ src/legend/SegmentOrientationLegend.js
@@ -19,15 +19,14 @@ export class SegmentOrientationLegend extends Legend {
         this.titleFontSize = opts.titleFontSize || '0.8em'
         this.titleFontWeight = opts.titleFontWeight || 'bold'
 
-        //exageration
-        this.exaggerationFactor = opts.exaggerationFactor || 0.5
-
-        //color
-        this.color = opts.color || 'gray'
         //orientation
         this.orientation = opts.orientation || 0
+        //color
+        this.color = opts.color || ((resolution, z) => 'gray')
         //width
-        this.widthPix = opts.widthPix || 3
+        this.width = opts.width || ((resolution, z) => 3 * z)
+        //length
+        this.length = opts.length || ((resolution, z) => resolution)
 
         //label
         this.label = opts.label || ''
@@ -54,24 +53,24 @@ export class SegmentOrientationLegend extends Legend {
         }
 
         //compute segment width and length, in pix
-        const sWidth = this.widthPix
-        const sLength = (1 * opts.resolution) / opts.z
+        const widthPix = this.width(opts.resolution, opts.z) / opts.z
+        const lengthPix = this.length(opts.resolution, opts.z) / opts.z
 
         //draw SVG segment
-        const svgS = Math.max(sLength, sWidth)
+        const svgS = Math.max(lengthPix, widthPix)
         const svg = d.append('svg').attr('width', svgS).attr('height', svgS).style('', 'inline-block')
 
         const cos = Math.cos((-this.orientation * Math.PI) / 180)
         const sin = Math.sin((-this.orientation * Math.PI) / 180)
         const dc = svgS * 0.5,
-            l2 = sLength * 0.5
+            l2 = lengthPix * 0.5
         svg.append('line')
             .attr('x1', dc - cos * l2)
             .attr('y1', dc - sin * l2)
             .attr('x2', dc + cos * l2)
             .attr('y2', dc + sin * l2)
             .style('stroke', this.color)
-            .style('stroke-width', sWidth)
+            .style('stroke-width', widthPix)
 
         //text label
         d.append('div')
