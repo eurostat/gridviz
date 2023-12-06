281045196455febbd87bdbe486a9470f1b7118c6
jgaffuri <jgaf@proton.me>
2023-12-06 10:01:55 +0100

    structure change

--- src/index.js
+++ src/index.js
@@ -47,10 +47,8 @@ export { LineLayer as BoundaryLayer } from './layer/LineLayer.js'
 export { ColorLegend } from './legend/ColorLegend.js'
 export { ColorDiscreteLegend } from './legend/ColorDiscreteLegend.js'
 export { ColorCategoryLegend } from './legend/ColorCategoryLegend.js'
-export { SizeLegend } from './legend/SizeLegend.js'
-//export { WidthLegend } from './legend/WidthLegend.js'
+export { SizeLegend, sizeLegend, sizeLegendViewScale, sizeDiscreteLegend, sizeDiscreteViewScaleLegend } from './legend/SizeLegend.js'
 export { OrientationLegend } from './legend/OrientationLegend.js'
-export { sizeLegend, sizeLegendViewScale, sizeDiscreteLegend, sizeDiscreteViewScaleLegend } from './legend/_SizeWidthLegendUtil.js'
 
 // export { goToStraight, zoomTo } from "./utils/zoomUtils"
 export * from './utils/stretching.js'
