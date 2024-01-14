eab2f0e5d4141c8b3cdab88fdc6bbd0c8f3b1df7
jgaffuri <jgaf@proton.me>
2024-01-14 20:45:48 +0100

    census 2021 demo

--- src/legend/TrivariateLegend.js
+++ src/legend/TrivariateLegend.js
@@ -16,6 +16,8 @@ export class TrivariateLegend extends Legend {
 
         //classifier
         this.classifier = opts.classifier
+
+        this.width = opts.width || 150
     }
 
     /**
@@ -29,7 +31,57 @@ export class TrivariateLegend extends Legend {
         //title
         this.makeTitle()
 
-        console.log("aaa")
+        const sqrt2over2 = 0.866025
+        const w = this.width, h = w * sqrt2over2
 
+        //make svg element
+        const svg = this.div
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h)
+
+        /*/triangle fill
+        const points = "0," + height + " " + this.width + "," + height + " " + (this.width / 2) + ",0"
+        svg.append('polygon')
+            .attr('points', points)
+            .attr('fill', this.classifier.centralColor)*/
+
+        //0 top
+        svg.append('polygon')
+            .attr('points', (w / 3) + "," + (h / 3) + " " + (w * 2 / 3) + "," + (h / 3) + " " + (w / 2) + ",0")
+            .attr('fill', this.classifier.colors[0])
+        //1 left
+        svg.append('polygon')
+            .attr('points', "0," + h + " " + (w / 3) + "," + h + " " + (w / 6) + "," + (2 * h / 3))
+            .attr('fill', this.classifier.colors[1])
+        //2 right
+        svg.append('polygon')
+            .attr('points', (w * 2 / 3) + "," + h + " " + w + "," + h + " " + (w * 5 / 6) + "," + (2 * h / 3))
+            .attr('fill', this.classifier.colors[2])
+
+        //middle triangle
+        svg.append('polygon')
+            .attr('points', (w / 2) + "," + (h / 3) + " " + (w / 4) + "," + (h * 5 / 6) + " " + (3 * w / 4) + "," + (h * 5 / 6))
+            .attr('fill', this.classifier.centralColor)
+
+        //12 bottom
+        svg.append('polygon')
+            .attr('points', (w / 3) + "," + (h / 3) + " " + (w * 2 / 3) + "," + (h / 3) + " " + (w / 2) + ",0")
+            .attr('fill', this.classifier.colors[0])
+
+
+
+
+
+        //
+        //TODO
+
+        /*/triangle stroke
+        svg.append('polygon')
+            .attr('points', points)
+            .attr('fill', "none")
+            .attr('stroke-width', 1)
+            .attr('stroke', "black")
+*/
     }
 }
