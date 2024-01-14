2b83e0a0b39973cd119ab7ff30170e248e64167a
jgaffuri <jgaf@proton.me>
2024-01-14 18:25:37 +0100

    trivairiate legend

---
+++ src/legend/TrivariateLegend.js
@@ -0,0 +1,35 @@
+//@ts-check
+'use strict'
+
+import { Legend } from '../core/Legend.js'
+
+/**
+ *
+ * @module legend
+ * @author Julien Gaffuri
+ */
+export class TrivariateLegend extends Legend {
+    /** @param {Object} opts */
+    constructor(opts) {
+        super(opts)
+        opts = opts || {}
+
+        //classifier
+        this.classifier = opts.classifier
+    }
+
+    /**
+     * @param {{} } opts
+     */
+    update(opts) {
+
+        //clear
+        this.div.selectAll('*').remove()
+
+        //title
+        this.makeTitle()
+
+        console.log("aaa")
+
+    }
+}
