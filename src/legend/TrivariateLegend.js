3eb4adc1866238d750e44912b3e4ca8daa8e7b44
jgaffuri <jgaf@proton.me>
2024-01-18 15:09:21 +0100

    trivariate map legend

--- src/legend/TrivariateLegend.js
+++ src/legend/TrivariateLegend.js
@@ -47,7 +47,7 @@ export class TrivariateLegend extends Legend {
         const selectionColor = this.selectionColor
         const selectionStrokeWidth = 0
         const tt = this.tooltip
-        const texts = this.texts
+        const texts = this.texts || {}
 
         const padding = 2
         const fontSize = 12
@@ -101,27 +101,27 @@ export class TrivariateLegend extends Legend {
         //trapezium s0
         const t0 = g.append('polygon')
             .attr('points', "0," + h + " " + w / 3 + "," + h + " " + w / 2 + "," + h * 2 / 3 + " " + w / 6 + "," + h * 2 / 3)
-        setAttributes(t0, classifier.colors[0], /*texts["0"]*/undefined)
+        setAttributes(t0, classifier.colors[0], texts["0"])
         //trapezium s1
         const t1 = g.append('polygon')
             .attr('points', w / 2 + ",0 " + w * 2 / 3 + "," + h / 3 + " " + w / 2 + "," + h * 2 / 3 + " " + w / 3 + "," + h / 3)
-        setAttributes(t1, classifier.colors[1], /*texts["0"]*/undefined)
+        setAttributes(t1, classifier.colors[1], texts["0"])
         //trapezium s2
         const t2 = g.append('polygon')
             .attr('points', w + "," + h + " " + w * 5 / 6 + "," + 2 * h / 3 + " " + w / 2 + "," + h * 2 / 3 + " " + w * 2 / 3 + "," + h)
-        setAttributes(t2, classifier.colors[2], /*texts["0"]*/undefined)
+        setAttributes(t2, classifier.colors[2], texts["0"])
         //triangle s0
         const t0_ = g.append('polygon')
             .attr('points', w / 2 + "," + h * 2 / 3 + " " + w * 5 / 6 + "," + h * 2 / 3 + " " + w * 2 / 3 + "," + h / 3)
-        setAttributes(t0_, classifier.middleColors[0], /*texts["0"]*/undefined)
+        setAttributes(t0_, classifier.middleColors[0], texts["0"])
         //triangle s1
         const t1_ = g.append('polygon')
             .attr('points', w / 2 + "," + h * 2 / 3 + " " + w / 3 + "," + h + " " + w * 2 / 3 + "," + h)
-        setAttributes(t1_, classifier.middleColors[1], /*texts["0"]*/undefined)
+        setAttributes(t1_, classifier.middleColors[1], texts["0"])
         //triangle s2
         const t2_ = g.append('polygon')
             .attr('points', w / 2 + "," + h * 2 / 3 + " " + w / 6 + "," + h * 2 / 3 + " " + w / 3 + "," + h / 3)
-        setAttributes(t2_, classifier.middleColors[2], /*texts["0"]*/undefined)
+        setAttributes(t2_, classifier.middleColors[2], texts["0"])
         //TODO
 
 
