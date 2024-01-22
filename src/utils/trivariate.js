4fdb5d2ce259ae958167eafb813c7f5e6213fe93
jgaffuri <jgaf@proton.me>
2024-01-22 14:51:08 +0100

    trivariate map

--- src/utils/trivariate.js
+++ src/utils/trivariate.js
@@ -50,15 +50,18 @@ export const trivariateColorClassifier = (properties, totalFunction, opts = {}) 
             return color2
         }
         if (s0 <= c0 && s1 >= c1 && s2 >= c2) {
-            if (cc && s0 > cc * c0) return centralColor
+            if (cc && s0 > cc * c0)
+                return centralColor
             return midColor0
         }
         if (s0 >= c0 && s1 <= c1 && s2 >= c2) {
-            if (cc && s1 > cc * c1) return centralColor
+            if (cc && s1 > cc * c1)
+                return centralColor
             return midColor1
         }
         if (s0 >= c0 && s1 >= c1 && s2 <= c2) {
-            if (cc && s2 > cc * c2) return centralColor
+            if (cc && s2 > cc * c2)
+                return centralColor
             return midColor2
         }
         return "black"
