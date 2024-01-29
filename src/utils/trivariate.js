6918a3ac27ce3ad2b1002037b48a09bf44d6f402
jgaffuri <jgaf@proton.me>
2024-01-29 16:36:48 +0100

    trivariate classifier refactoring

--- src/utils/trivariate.js
+++ src/utils/trivariate.js
@@ -4,34 +4,20 @@
 import { interpolateLab } from "d3-interpolate"
 
 
-
-
 export const trivariateClassifier = (properties, totalFunction, opts = {}) => {
-
-    //the center point coordinates. sum must be equal to 1
-    const [c0, c1, c2] = opts.center || [1 / 3, 1 / 3, 1 / 3]
-
-    //parameter to decide wether to show middle classes. Middle classes are the 3 intersection classes
-    const withMixedClasses = opts.withMixedClasses != undefined ? opts.withMixedClasses : true
-
-
-    //the three classes
-    //const [class0, class1, class2] = opts.colors || ["red", "green", "blue"]
-    //the color interpolation function
-    //const colorInterpolation = opts.colorInterpolation || interpolateLab
-
-    //https://d3js.org/d3-interpolate/color
-    //const middleColorFunction = (color1, color2) => colorInterpolation(color1, color2)(0.5)
-    //the colors corresponding to the middle classes
-    //const [midColor0, midColor1, midColor2] = opts.middleColors || withMixedClasses ? [middleColorFunction(class1, class2), middleColorFunction(class0, class2), middleColorFunction(class0, class1)] : []
-
-    //the central color, used for the central class, if any. The central class is the class of relatively balanced values, around the center point
-    //const centralColor = opts.centralColor || colorInterpolation(middleColorFunction(class0, class1), class2)(0.333)
-    //the coefficient for the size of the central class. Set to 0 or undefined for not showing any central class. Set to 1 for a central class that contains the middle classes
-    const cc = opts.centerCoefficient ? 1 - opts.centerCoefficient : undefined
 
     //the three properties
     const p0 = properties[0], p1 = properties[1], p2 = properties[2]
+
+    //the classifier center point. sum must be equal to 1
+    const [c0, c1, c2] = opts.center || [1 / 3, 1 / 3, 1 / 3]
+
+    //parameter to decide wether to use mixed classes m0, m1, m2.
+    const withMixedClasses = opts.withMixedClasses != undefined ? opts.withMixedClasses : true
+
+    //paramerter decide wether to use a central class, and the size of this central class.
+    //set to 0 or undefined for not showing any central class. Set to 1 for a central class that contains the mix classes
+    const cc = opts.centerCoefficient ? 1 - opts.centerCoefficient : undefined
 
     //the output classifier method
     const fun = c => {
@@ -45,57 +31,86 @@ export const trivariateClassifier = (properties, totalFunction, opts = {}) => {
         if (s0 >= c0 && s1 <= c1 && s2 <= c2) {
             //central class near class 0
             if (cc != undefined && (s2 - c2) * (c1 - cc * c1) >= (s1 - cc * c1) * (cc * c2 - c2))
-                return centralColor
-            return class0
+                return "center"
+            return "0"
         }
         //class 1
         if (s0 <= c0 && s1 >= c1 && s2 <= c2) {
             //central class near class 1
             if (cc != undefined && (s2 - c2) * (c0 - cc * c0) >= (s0 - cc * c0) * (cc * c2 - c2))
-                return centralColor
-            return class1
+                return "center"
+            return "1"
         }
         //class 2
         if (s0 <= c0 && s1 <= c1 && s2 >= c2) {
             //central class near class 2
             if (cc != undefined && (s1 - c1) * (c0 - cc * c0) >= (s0 - cc * c0) * (cc * c1 - c1))
-                return centralColor
-            return class2
+                return "center"
+            return "2"
         }
         //middle class 0 - intersection class 1 and 2
         if (s0 <= c0 && s1 >= c1 && s2 >= c2) {
             //central class
-            if (cc != undefined && s0 > cc * c0) return centralColor
-            if (withMixedClasses) return midColor0
-            return s1 > s2 ? class1 : class2
+            if (cc != undefined && s0 > cc * c0) return "center"
+            if (withMixedClasses) return "m0"
+            return s1 > s2 ? "1" : "2"
         }
         //middle class 1 - intersection class 0 and 1
         if (s0 >= c0 && s1 <= c1 && s2 >= c2) {
             //central class
-            if (cc != undefined && s1 > cc * c1) return centralColor
-            if (withMixedClasses) return midColor1
-            return s0 > s2 ? class0 : class2
+            if (cc != undefined && s1 > cc * c1) return "center"
+            if (withMixedClasses) return "m1"
+            return s0 > s2 ? "0" : "2"
         }
         //middle class 2 - intersection class 0 and 1
         if (s0 >= c0 && s1 >= c1 && s2 <= c2) {
             //central class
-            if (cc != undefined && s2 > cc * c2) return centralColor
-            if (withMixedClasses) return midColor2
-            return s1 > s0 ? class1 : class0
+            if (cc != undefined && s2 > cc * c2) return "center"
+            if (withMixedClasses) return "m2"
+            return s1 > s0 ? "1" : "0"
         }
         //should not happen
-        return opts.defaultColor || "black"
+        return "unknown"
     }
 
     //attach information to output function
     fun.center = [c0, c1, c2]
-    fun.colors = [class0, class1, class2]
-    fun.middleColors = [midColor0, midColor1, midColor2]
-    fun.centralColor = centralColor
     fun.centerCoefficient = opts.centerCoefficient
 
     return fun
 }
+
+
+export const trivariateColorClassifier = (properties, totalFunction, opts = {}) => {
+
+}
+
+
+
+
+//the three classes
+//const [class0, class1, class2] = opts.colors || ["red", "green", "blue"]
+//the color interpolation function
+//const colorInterpolation = opts.colorInterpolation || interpolateLab
+
+//https://d3js.org/d3-interpolate/color
+//const middleColorFunction = (color1, color2) => colorInterpolation(color1, color2)(0.5)
+//the colors corresponding to the middle classes
+//const [midColor0, midColor1, midColor2] = opts.middleColors || withMixedClasses ? [middleColorFunction(class1, class2), middleColorFunction(class0, class2), middleColorFunction(class0, class1)] : []
+
+//the central color, used for the central class, if any. The central class is the class of relatively balanced values, around the center point
+//const centralColor = opts.centralColor || colorInterpolation(middleColorFunction(class0, class1), class2)(0.333)
+/*
+fun.colors = [class0, class1, class2]
+fun.middleColors = [midColor0, midColor1, midColor2]
+fun.centralColor = centralColor
+*/
+
+
+
+
+
+
 
 
 
