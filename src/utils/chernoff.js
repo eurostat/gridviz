3028766962115c6a77af1e3c5216eed006f514e8
jgaffuri <jgaf@proton.me>
2024-01-31 13:39:37 +0100

    chernoff faces

--- src/utils/chernoff.js
+++ src/utils/chernoff.js
@@ -3,7 +3,16 @@
 
 import { loadImage } from '../utils/utils.js'
 
-//load chernoff images
+//
+
+/**
+ * Load chernoff images
+ * See for example: https://github.com/jgaffuri/chernoff-faces
+ * @param {object} out 
+ * @param {string} baseURL 
+ * @param {function|undefined} callback 
+ * @returns 
+ */
 export async function loadChernoffFacesImages(out, baseURL, callback = undefined) {
     out = out || {}
     for (let s of ["f", "m"])
@@ -11,4 +20,5 @@ export async function loadChernoffFacesImages(out, baseURL, callback = undefined
             for (let i = 0; i <= 2; i++)
                 out[s + a + i] = await loadImage(baseURL + (s + a + i) + ".png")
     if (callback) callback()
+    return out
 }
