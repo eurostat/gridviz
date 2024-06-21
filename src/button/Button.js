6c42d97f81db04ed97c6181322e88a43484543aa
joewdavies <jwd101@gmail.com>
2024-06-21 12:07:42 +0200

    add ids to button elements

--- src/button/Button.js
+++ src/button/Button.js
@@ -18,16 +18,15 @@ export class Button {
      * opts.y
      */
     constructor(opts = {}) {
-
         this.map = opts.map
         this.parentNode = opts.parentNode || opts.map.container
 
         // the div element
-        if (this.id) this.div = select('#' + this.id)
+        if (opts.id) this.div = select('#' + opts.id)
 
         if (!this.div || this.div.empty()) {
             this.div = select(document.createElement('div'))
-            if (this.id) this.div.attr('id', this.id)
+            if (opts.id) this.div.attr('id', opts.id)
         }
 
         if (opts.title) this.div.attr('title', opts.title)
@@ -37,7 +36,10 @@ export class Button {
         if (opts.onClickFunction) this.div.on('click', opts.onClickFunction)
 
         //set styles
-        this.style('box-shadow', '0 7px 8px rgba(0,47,103,.08), 0 0 22px rgba(0,47,103,.04), 0 12px 17px rgba(0,47,103,.04), 0 -4px 4px rgba(0,47,103,.04)') //.ecl-u-shadow-3
+        this.style(
+            'box-shadow',
+            '0 7px 8px rgba(0,47,103,.08), 0 0 22px rgba(0,47,103,.04), 0 12px 17px rgba(0,47,103,.04), 0 -4px 4px rgba(0,47,103,.04)'
+        ) //.ecl-u-shadow-3
         this.style('background-color', '#ffffff')
         this.style('position', 'absolute')
         this.style('cursor', 'pointer')
@@ -47,7 +49,6 @@ export class Button {
         this.style('width', '30px')
         this.style('height', '30px')
         // this.style(padding , '4px'
-
 
         // append to parent
         this.parentNode.appendChild(this.div.node())
