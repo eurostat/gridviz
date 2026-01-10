566f0bcac2864e39e672fc1dff11e360e0fdcc60
jgaffuri <jgaf@proton.me>
2026-01-10 20:59:59 +0100

    a

--- src/style/Interpolator.js
+++ src/style/Interpolator.js
@@ -58,21 +58,24 @@ export class Interpolator extends Style {
         let m = cellsToMatrix(cells, resolution, c => this.value(c))
         const x0 = m.x0, y0 = m.y0
 
-        //get target resolution
-        const targetResolution = this.targetResolution(resolution, z)
+        //get target resolution and scale factor
+        let targetResolution = this.targetResolution(resolution, z)
+        const scaleFactor = Math.round(resolution / targetResolution)
+        targetResolution = resolution / scaleFactor
 
         // compute ray casting shadow
-        m = bilinearInterpolator(m, Math.round(resolution / targetResolution));
+        m = bilinearInterpolator(m, scaleFactor);
 
         // make cells
         cells = []
+        const ra = (resolution - targetResolution) / 2
         for (let i = 0; i < m.length; i++) {
             const row = m[i]
-            const y = y0 - i * targetResolution
+            const y = y0 - i * targetResolution + ra
             for (let j = 0; j < row.length; j++) {
                 const v = row[j]
                 if (!v) continue
-                const c = { x: x0 + j * targetResolution, y: y }
+                const c = { x: x0 + j * targetResolution + ra, y: y }
                 c[this.interpolatedProperty] = v
                 cells.push(c)
             }
@@ -111,34 +114,83 @@ export class Interpolator extends Style {
 
 
 function bilinearInterpolator(grid, scaleFactor = 5) {
+    console.log('bilinearInterpolator scaleFactor=', scaleFactor)
+    if (scaleFactor === 1) return grid;
     const rows = grid.length;
     if (rows === 0) return [];
     const cols = grid[0].length;
     const fineRows = rows * scaleFactor;
     const fineCols = cols * scaleFactor;
     const fineGrid = Array(fineRows).fill().map(() => Array(fineCols).fill(undefined));
+
+    //const sfIsEven = scaleFactor % 2 === 0
+
+    //compute output grid values
     for (let i = 0; i < fineRows; i++) {
+
         const coarseI = Math.min(Math.floor(i / scaleFactor), rows - 2);
+        const yRatio = (i % scaleFactor) / scaleFactor;
+        const i_ = coarseI * scaleFactor + (scaleFactor / 2)
+
         for (let j = 0; j < fineCols; j++) {
             const coarseJ = Math.min(Math.floor(j / scaleFactor), cols - 2);
+            const xRatio = (j % scaleFactor) / scaleFactor;
+            const j_ = coarseJ * scaleFactor + (scaleFactor / 2)
+
+            // get four corner values
             const topLeft = grid[coarseI][coarseJ];
             const topRight = grid[coarseI][coarseJ + 1];
             const bottomLeft = grid[coarseI + 1][coarseJ];
-            const bottomRight = grid[coarseI + 1][coarseJ + 1]; // If all four are defined, interpolate normally
+            const bottomRight = grid[coarseI + 1][coarseJ + 1];
+
+            /*
+            if (sfIsEven) {
+                //TODO
+                if (topLeft === undefined && i <= i_ && j <= j_) continue
+            } else {
+                if (topLeft === undefined && i <= i_ && j <= j_) continue
+                if (topRight === undefined && i <= i_ && j >= j_) continue
+                if (bottomLeft === undefined && i >= i_ && j <= j_) continue
+                if (bottomRight === undefined && i >= i_ && j >= j_) continue
+            }*/
+
+            // If all four are defined, interpolate normally
             if (topLeft !== undefined && topRight !== undefined && bottomLeft !== undefined && bottomRight !== undefined) {
-                const xRatio = (i % scaleFactor) / scaleFactor;
-                const yRatio = (j % scaleFactor) / scaleFactor;
-                const top = topLeft + (topRight - topLeft) * yRatio;
-                const bottom = bottomLeft + (bottomRight - bottomLeft) * yRatio;
-                fineGrid[i][j] = top + (bottom - top) * xRatio;
-            } // If only two diagonally opposite points are defined, interpolate along diagonal
+                const top = topLeft + (topRight - topLeft) * xRatio;
+                const bottom = bottomLeft + (bottomRight - bottomLeft) * xRatio;
+                fineGrid[i][j] = top + (bottom - top) * yRatio;
+            }
+            // If only one is not defined, interpolate using the other three
+            /*else if (topLeft === undefined && topRight !== undefined && bottomLeft !== undefined && bottomRight !== undefined) {
+                //const bottom = bottomLeft + (bottomRight - bottomLeft) * xRatio;
+                //fineGrid[i][j] = bottom;
+            }*/
+            // If only two diagonally opposite points are defined, interpolate along diagonal
             else if (topLeft !== undefined && bottomRight !== undefined) {
-                //TODO: improve diagonal interpolation ? Do not take the middle of the two points but interpolate according to position
-                fineGrid[i][j] = (topLeft + bottomRight) / 2;
+                //const top = topLeft + (bottomRight - topLeft) * xRatio;
+                //const bottom = topLeft + (bottomRight - topLeft) * xRatio;
+                //fineGrid[i][j] = top + (bottom - top) * yRatio;
+                fineGrid[i][j] = topLeft + (bottomRight - topLeft) * ((xRatio + yRatio) / 2);
             } else if (topRight !== undefined && bottomLeft !== undefined) {
-                //TODO: improve diagonal interpolation ? Do not take the middle of the two points but interpolate according to position
-                fineGrid[i][j] = (topRight + bottomLeft) / 2;
-            } // If only one point is defined, use that value
+                //const top = topRight + (bottomLeft - topRight) * xRatio;
+                //const bottom = topRight + (bottomLeft - topRight) * xRatio;
+                //fineGrid[i][j] = top + (bottom - top) * yRatio;
+                fineGrid[i][j] = topRight + (bottomLeft - topRight) * ((xRatio + yRatio) / 2);
+            }
+            // If only two adjacent points are defined, interpolate between them
+            else if (topLeft !== undefined && bottomLeft !== undefined) {
+                fineGrid[i][j] = topLeft + (bottomLeft - topLeft) * yRatio;
+            }
+            else if (topRight !== undefined && bottomRight !== undefined) {
+                fineGrid[i][j] = topRight + (bottomRight - topRight) * yRatio;
+            }
+            else if (topLeft !== undefined && topRight !== undefined) {
+                fineGrid[i][j] = topLeft + (topRight - topLeft) * xRatio;
+            }
+            else if (bottomLeft !== undefined && bottomRight !== undefined) {
+                fineGrid[i][j] = bottomLeft + (bottomRight - bottomLeft) * xRatio;
+            }
+            // If only one point is defined, use that value
             else if (topLeft !== undefined) {
                 fineGrid[i][j] = topLeft;
             } else if (topRight !== undefined) {
@@ -147,9 +199,6 @@ function bilinearInterpolator(grid, scaleFactor = 5) {
                 fineGrid[i][j] = bottomLeft;
             } else if (bottomRight !== undefined) {
                 fineGrid[i][j] = bottomRight;
-            } // Otherwise, leave as undefined
-            else {
-                fineGrid[i][j] = undefined;
             }
         }
     }
